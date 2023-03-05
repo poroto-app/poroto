132060d382b2256dfefcaea0208ae7333e69c8d5
masakizk <robomasaki2@gmail.com>
2023-03-05 19:30:52 +0900

    feat: chakra provider

---
+++ src/pages/_app.tsx
@@ -0,0 +1,67 @@
+import {AppProps} from "next/app";
+import Head from "next/head";
+import {ChakraProvider} from "@chakra-ui/react";
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8"/>
+                <title>poroto</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            </Head>
+            <style jsx global>
+                {`
+                  html {
+                    font-family: Helvetica, Hiragino Kaku Gothic Pro, Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
+                  }
+
+                  button {
+                    border-style: none;
+                    background-color: transparent;
+                    font-size: inherit;
+                  }
+
+                  button:focus {
+                    outline: none;
+                  }
+
+                  html,
+                  body,
+                  #__next {
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                    min-width: 370px;
+                  }
+
+                  h1,
+                  h2,
+                  h3,
+                  h4,
+                  h5,
+                  h6 {
+                    margin: 0;
+                  }
+
+                  a {
+                    text-decoration: none;
+                    color: inherit;
+                  }
+
+                  a:hover {
+                    cursor: pointer;
+                  }
+
+                  ol,
+                  ul {
+                    list-style: none;
+                  }
+                `}
+            </style>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
+    )
+}
