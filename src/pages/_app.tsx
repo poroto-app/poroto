bcc03eeb89f51662aac51c7b266d42bdbee256e0
ishizakamasaki <robomasaki2@gmail.com>
2023-03-14 08:28:36 +0900

    feat: `yarn tsc`をすると環境によって、global styleについて型エラーがでるので修正
    以下のようなエラーが出てしまう。
    ```
    Property 'jsx' does not exist on type 'DetailedHTMLProps<StyleHTMLAttributes<HTMLStyleElement>, HTMLStyleElement>'.
    ```

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ export default function App({Component, pageProps}: AppProps) {
                 <title>poroto</title>
                 <meta name="viewport" content="width=device-width,initial-scale=1"/>
             </Head>
+            {/*@ts-ignore*/}
             <style jsx global>
                 {`
                   html {
