dfdb07f1a111fb6e1b004a68cb409e4d52f25cf6
ishizakamasaki <robomasaki2@gmail.com>
2023-03-20 21:11:23 +0900

    feat: ボタンを押すと現在地を取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,17 @@ import {Container} from "@chakra-ui/react";
 import {CreatePlanFromCurrentLocationButton} from "src/view/top/CreatePlanFromCurrentLocationButton";
 import {useRouter} from "next/router";
 import {Routes} from "src/view/constants/router";
+import {useLocation} from "src/view/hooks/useLocation";
 
 const IndexPage = () => {
     const router = useRouter();
+    const {getCurrentLocation} = useLocation();
 
-    const onClickCreatePlanFromCurrentLocation = () => {
-        router.push(Routes.plans.select).then()
+    const onClickCreatePlanFromCurrentLocation = async () => {
+        const currentLocation = await getCurrentLocation();
+        console.log(currentLocation);
+        await router.push(Routes.plans.select);
     }
-
 
     return <Container maxW="990px" px="16px" py="16px">
         <CreatePlanFromCurrentLocationButton onClick={onClickCreatePlanFromCurrentLocation}/>
