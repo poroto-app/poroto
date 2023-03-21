d6a6b577cd57056c180b344a4c46146a97af6813
ishizakamasaki <robomasaki2@gmail.com>
2023-03-21 09:19:34 +0900

    feat: hookに関係の無いコードを外に出す。

--- src/view/hooks/useLocation.ts
+++ src/view/hooks/useLocation.ts
@@ -1,27 +1,28 @@
 import {useState} from "react";
 import {GeoLocation} from "src/domain/models/GeoLocation";
 
+
+const fetchCurrentLocation = async (): Promise<GeoLocation | null> => {
+    if (!navigator.geolocation) {
+        console.info("cannot use the geolocation API");
+        return null;
+    }
+
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            resolve({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            });
+        }, (e) => {
+            reject(e)
+        })
+    });
+}
+
 export const useLocation = () => {
     const [location, setLocation] = useState<GeoLocation>(null);
     const [isLoadingLocation, setIsLoadingLocation] = useState(false);
-
-    const fetchCurrentLocation = async (): Promise<GeoLocation | null> => {
-        if (!navigator.geolocation) {
-            console.info("cannot use the geolocation API");
-            return null;
-        }
-
-        return new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition((position) => {
-                resolve({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-            }, (e) => {
-                reject(e)
-            })
-        });
-    }
 
     const fetchCurrentLocationWithHook = async (): Promise<GeoLocation | null> => {
         setIsLoadingLocation(true);
