a9d3e3961c60aaae71f3a840e7a98e94625b41e2
masakizk <robomasaki2@gmail.com>
2023-03-21 11:14:39 +0900

    feat: ロゴアニメーションを作成

--- src/view/common/LoadingModal.tsx
+++ src/view/common/LoadingModal.tsx
@@ -1,4 +1,6 @@
 import {Center, Text, VStack} from "@chakra-ui/react";
+import Image from "next/image";
+import styled, {keyframes} from "styled-components";
 
 type Props = {
     title: string
@@ -11,7 +13,9 @@ export const LoadingModal = ({title}: Props) => {
         position="fixed" top={0} right={0} bottom={0} left={0}
     >
         <VStack>
-            <img src="/images/logo.svg" alt="logo"/>
+            <FadeInFadeOutTransition>
+                <Image width={100} height={100} src="/images/logo.svg" alt="logo"/>
+            </FadeInFadeOutTransition>
             <Text
                 px="16px" mt="8px" maxW="100%" fontSize="1.25rem"
                 bgGradient="linear(45deg, #4E6382, #BBA0A6)" bgClip="text"
@@ -19,3 +23,28 @@ export const LoadingModal = ({title}: Props) => {
         </VStack>
     </Center>
 }
+
+const fadeinFadeOut = keyframes`
+  0% {
+    opacity: .1;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const FadeInFadeOutTransition = styled.div`
+  animation: ${fadeinFadeOut} 0.75s cubic-bezier(0.25, 0.46, 0.45, 0.94) alternate-reverse infinite,
+  ${rotate} 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite;
+`;
