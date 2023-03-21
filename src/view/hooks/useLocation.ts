21e4791aab0b0568fd30d36dd2488b07c11e0a00
ishizakamasaki <robomasaki2@gmail.com>
2023-03-21 09:18:42 +0900

    feat: hookで現在地と読込中であることがわかるように修正

--- src/view/hooks/useLocation.ts
+++ src/view/hooks/useLocation.ts
@@ -1,7 +1,11 @@
+import {useState} from "react";
 import {GeoLocation} from "src/domain/models/GeoLocation";
 
 export const useLocation = () => {
-    const getCurrentLocation = async (): Promise<GeoLocation | null> => {
+    const [location, setLocation] = useState<GeoLocation>(null);
+    const [isLoadingLocation, setIsLoadingLocation] = useState(false);
+
+    const fetchCurrentLocation = async (): Promise<GeoLocation | null> => {
         if (!navigator.geolocation) {
             console.info("cannot use the geolocation API");
             return null;
@@ -19,7 +23,23 @@ export const useLocation = () => {
         });
     }
 
+    const fetchCurrentLocationWithHook = async (): Promise<GeoLocation | null> => {
+        setIsLoadingLocation(true);
+        try {
+            const currentLocation = await fetchCurrentLocation();
+            setLocation(currentLocation);
+            return currentLocation;
+        } catch (e) {
+            setLocation(null);
+            return null;
+        } finally {
+            setIsLoadingLocation(false);
+        }
+    }
+
     return {
-        getCurrentLocation
+        isLoadingLocation,
+        location,
+        getCurrentLocation: fetchCurrentLocationWithHook,
     }
 }
