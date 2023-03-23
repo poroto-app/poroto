7e6915216c25e1ccb89fc9960f8fdaf042862b79
masakizk <robomasaki2@gmail.com>
2023-03-23 22:14:53 +0900

    feat(redux): redux stateとその更新のactionを定義

--- src/redux/plan.ts
+++ src/redux/plan.ts
@@ -1,13 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {PlanEntry} from "src/domain/plan/Plan";
 
-export type PlanState = {}
+export type PlanState = {
+    plans: PlanEntry[] | null,
+}
 
-const initialState: PlanState = {}
+const initialState: PlanState = {
+    plans: null,
+}
 
 export const slice = createSlice({
     name: 'plan',
     initialState,
-    reducers: {},
+    reducers: {
+        setPlans: (state, {payload}: PayloadAction<{ plans: PlanEntry[] | null }>) => {
+            state.plans = payload.plans;
+        },
+    },
     extraReducers: (builder) => {
 
     },
