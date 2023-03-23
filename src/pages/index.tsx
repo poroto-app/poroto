369b1613329a691ea6ff5fb89838287361ce8bfe
masakizk <robomasaki2@gmail.com>
2023-03-23 22:27:15 +0900

    feat(redux, view): アプリの状態を反映したdispatcherを定義

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import {Container} from "@chakra-ui/react";
 import {CreatePlanFromCurrentLocationButton} from "src/view/top/CreatePlanFromCurrentLocationButton";
 import {useRouter} from "next/router";
 import {Routes} from "src/view/constants/router";
+import {useAppDispatch} from "src/redux/redux";
 
 const IndexPage = () => {
     const router = useRouter();
+    const dispatch = useAppDispatch();
 
     const onClickCreatePlanFromCurrentLocation = () => {
         router.push(Routes.plans.select).then()
