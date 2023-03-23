e8e5bcbc7c9b889d2f056b08c30acacdc4dfcd6b
masakizk <robomasaki2@gmail.com>
2023-03-23 22:33:13 +0900

    feat(view): ボタンをクリックするとプラン取得処理を発火

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,15 @@ import {CreatePlanFromCurrentLocationButton} from "src/view/top/CreatePlanFromCu
 import {useRouter} from "next/router";
 import {Routes} from "src/view/constants/router";
 import {useAppDispatch} from "src/redux/redux";
+import {createPlanFromLocation} from "src/redux/plan";
 
 const IndexPage = () => {
     const router = useRouter();
     const dispatch = useAppDispatch();
 
     const onClickCreatePlanFromCurrentLocation = () => {
+        // TODO: 現在地を取得する
+        dispatch(createPlanFromLocation({location: {latitude: 0, longitude: 0}}))
         router.push(Routes.plans.select).then()
     }
 
