9489b968ad3cfb16268390cac6dedfa3f427dd2f
zacker <robomasaki2@gmail.com>
2023-04-15 20:21:01 +0900

    feat(view): ボタンがクリックされた後の活性状態が長くなるようにする

--- src/view/interest/SelectButton.tsx
+++ src/view/interest/SelectButton.tsx
@@ -1,10 +1,20 @@
 import {IconType} from "react-icons";
 import {Icon} from "@chakra-ui/react";
 import styled from "styled-components";
-import React from "react";
+import React, {useRef} from "react";
 
 export const SelectButton = ({color, onClick, icon}: { color: string, onClick: () => void, icon: IconType }) => {
-    return <Button color={color} onClick={onClick}>
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const handleOnClick = () => {
+        // MEMO: ボタンにCSSのactive状態が適用されるのはボタンを押している間だけなので、
+        // Refを使ってフォーカス状態を長めに発生させる
+        buttonRef.current.focus();
+        setTimeout(() => buttonRef?.current?.blur(), 200);
+        onClick();
+    }
+
+    return <Button color={color} ref={buttonRef} onClick={handleOnClick}>
         <Icon w="32px" h="32px" color={color} as={icon}/>
     </Button>
 }
@@ -15,15 +25,16 @@ const Button = styled.button<{ color: string }>`
   flex: 1;
   justify-content: center;
   align-items: center;
+  -webkit-tap-highlight-color: transparent;
 
   background-color: white;
   border: 2px solid ${({color}) => color};
   border-radius: 100px;
   padding: 8px;
 
-  transition: all 0.2s;
+  transition: all 200ms;
 
-  &:active {
+  &:focus {
     background-color: ${({color}) => color};
 
     > svg {
