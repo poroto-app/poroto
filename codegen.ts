7c5e4f2637fd3e6cf23f27a757ca6a2d7281999c
zacker <robomasaki2@gmail.com>
2023-04-16 18:37:18 +0900

    feat(graphql): クライアントサイドで利用するコードが生成されるようにする

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+import type {CodegenConfig} from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+    overwrite: true,
+    // TODO: GitHubから取得できるようにする
+    schema: "http://localhost:8080/graphql",
+    documents: "graphql/documents/**.graphql",
+    generates: {
+        "src/graphql/graphql.ts": {
+            plugins: [
+                "typescript",
+                "typescript-operations",
+                "typed-document-node",
+            ],
+        },
+    }
+};
+
+export default config;
