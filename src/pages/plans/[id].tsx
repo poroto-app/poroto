c1f422c3a84a9f038201f2b59e8e05e05aa7f17c
zacker <robomasaki2@gmail.com>
2023-04-22 21:26:08 +0900

    feat(view): プラン詳細画面に所要時間が表示

--- src/pages/plans/[id].tsx
+++ src/pages/plans/[id].tsx
@@ -1,11 +1,14 @@
-import {Box, Center, VStack} from "@chakra-ui/react"
+import {Box, Center, HStack, Icon, Text, VStack} from "@chakra-ui/react"
 import {NavBar} from "src/view/common/NavBar"
 import {PlacePreview} from "src/view/plan/PlacePreview"
 import {useAppDispatch} from "src/redux/redux";
 import {fetchPlanDetail, reduxPlanSelector} from "src/redux/plan";
 import {LoadingModal} from "src/view/common/LoadingModal";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {useRouter} from "next/router";
+import {IconType} from "react-icons";
+import {MdSchedule} from "react-icons/all";
+import {DateHelper} from "src/domain/util/date";
 
 const PlanDetail = () => {
 
@@ -24,6 +27,12 @@ const PlanDetail = () => {
     return <Center flexDirection="column">
         <NavBar title={plan.title}/>
         <Box maxWidth="990px" w="100%" px="8px" py="16px" boxSizing="border-box">
+            <VStack py="16px">
+                <PlanSummaryItem
+                    icon={MdSchedule}
+                    text={`${plan.timeInMinutes.toFixed()}分 (~ ${DateHelper.dateToHHMM(DateHelper.add(new Date(), plan.timeInMinutes * DateHelper.Minute))})`}
+                />
+            </VStack>
             <VStack spacing={8} w="100%">
                 {
                     plan.places.map((place, i) => <PlacePreview
@@ -36,6 +45,13 @@ const PlanDetail = () => {
             </VStack>
         </Box>
     </Center>
+}
+
+const PlanSummaryItem = ({text, icon}: { text: string, icon: IconType }) => {
+    return <HStack w="100%" px="16px" py="4px" columnGap="20px" spacing={0}>
+        <Icon w="24px" h="24px" color="#BD9F8E" as={icon}/>
+        <Text color="rgba(0,0,0,.6)">{text}</Text>
+    </HStack>
 }
 
 export default PlanDetail
