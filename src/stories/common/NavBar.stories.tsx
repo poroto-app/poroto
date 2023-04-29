427333ded7004a9720df3bfa3d87fa765a753125
zacker <robomasaki2@gmail.com>
2023-04-29 17:11:39 +0900

    feat: 戻るボタンを利用できるようにする

--- src/stories/common/NavBar.stories.tsx
+++ src/stories/common/NavBar.stories.tsx
@@ -1,17 +1,18 @@
 import {ComponentMeta, ComponentStory} from "@storybook/react";
-import {NavBar} from "src/view/common/NavBar";
+import {NavBarComponent} from "src/view/common/NavBar";
 import {Box} from "@chakra-ui/react";
 
 export default {
     title: "common/NavBar",
-    component: NavBar,
-} as ComponentMeta<typeof NavBar>;
+    component: NavBarComponent,
+} as ComponentMeta<typeof NavBarComponent>;
 
-const Template: ComponentStory<typeof NavBar> = (args) => <Box w="100%">
-    <NavBar title={args.title}/>
+const Template: ComponentStory<typeof NavBarComponent> = (args) => <Box w="100%">
+    <NavBarComponent title={args.title} canBack={args.canBack} onBack={() => 0}/>
 </Box>
 
 export const NavBarStoryBook = Template.bind({});
 NavBarStoryBook.args = {
-    title: "Navigation Bar Title"
+    title: "Navigation Bar Title",
+    canBack: true,
 }
--- src/view/common/NavBar.tsx
+++ src/view/common/NavBar.tsx
@@ -1,16 +1,55 @@
 import styled from "styled-components";
-import {HStack, Text} from "@chakra-ui/react";
-import React from "react";
+import {HStack, Icon, Text} from "@chakra-ui/react";
+import React, {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {MdArrowBack, MdOutlinePlace} from "react-icons/md";
+import {reduxHistorySelector} from "src/redux/history";
 
 type Props = {
     title?: string
 }
 
 export const NavBar = ({title}: Props) => {
+    const router = useRouter();
+    const [isHome, setIsHome] = useState(false);
+
+    useEffect(() => {
+        setIsHome(router.pathname === "/");
+    }, [router.pathname]);
+
+    const handleOnBack = async () => {
+        const isHome = router.pathname === "/";
+        if (isHome) return;
+
+        // TODO: ホーム以外のページに直接来たときに、
+        // 通常の戻る操作ではなく、ホームに飛ばすようにする。
+        // (直接来た場合は、戻るボタンを押した場合、その前に見ていたページに遷移するため)
+        await router.back();
+    }
+
+    // const router = useRouter();
+    return <NavBarComponent
+        title={title}
+        canBack={!isHome}
+        onBack={handleOnBack}
+    />
+}
+
+export const NavBarComponent = ({title, canBack, onBack}: Props & {
+    canBack: boolean,
+    onBack: () => void,
+}) => {
     return <Container>
-        <HStack w="100%" maxW="990px">
+        <HStack w="100%" maxW="990px" spacing={4}>
             {
-                title && <Text>{title}</Text>
+                canBack && <Icon
+                    w="20px" h="20px" as={MdArrowBack}
+                    cursor="pointer"
+                    onClick={onBack}
+                />
+            }
+            {
+                title && <Text fontSize="18px" userSelect="none">{title}</Text>
             }
         </HStack>
     </Container>
