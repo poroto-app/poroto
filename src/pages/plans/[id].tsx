633047a8c8b9faed1288813d278bb3f887aebbf4
zacker <robomasaki2@gmail.com>
2023-05-06 16:26:24 +0900

    feat(view): Google Mapで経路検索をするためのボタンを作成

--- src/pages/plans/[id].tsx
+++ src/pages/plans/[id].tsx
@@ -1,4 +1,4 @@
-import {Box, Center, HStack, Icon, Text, VStack} from "@chakra-ui/react"
+import {Center, HStack, Icon, Text, VStack} from "@chakra-ui/react"
 import {NavBar} from "src/view/common/NavBar"
 import {PlacePreview} from "src/view/plan/PlacePreview"
 import {useAppDispatch} from "src/redux/redux";
@@ -7,11 +7,11 @@ import {LoadingModal} from "src/view/common/LoadingModal";
 import {useEffect, useRef} from "react";
 import {useRouter} from "next/router";
 import {PlanActionButton} from "src/view/plan/Props";
-import {MdPhotoCamera} from "react-icons/md";
+import {MdPhotoCamera, MdSchedule} from "react-icons/md";
 import html2canvas from "html2canvas";
 import {IconType} from "react-icons";
-import {MdSchedule} from "react-icons/md";
 import {DateHelper} from "src/domain/util/date";
+import Link from "next/link";
 
 const PlanDetail = () => {
 
@@ -46,6 +46,25 @@ const PlanDetail = () => {
         }
     }
 
+    const generateGoogleMapUrl = (): string => {
+        // SEE: https://developers.google.com/maps/documentation/urls/get-started?hl=ja#directions-action
+        const url = new URL("https://www.google.com/maps/dir/");
+        url.searchParams.set("api", "1");
+
+        // TODO: 出発地点が現在地でなく、検索した場所のときは明示的に出発地点を設定する
+        // baseUrl.searchParams.set("origin", `${plan.places[0].location.latitude},${plan.places[0].location.longitude}`);
+
+        const waypoints = plan.places.slice(0, plan.places.length - 1)
+            .map((waypoint) => `${waypoint.location.longitude},${waypoint.location.latitude}`)
+            .join("|");
+        url.searchParams.set("waypoints", waypoints)
+
+        const destination = plan.places[plan.places.length - 1];
+        url.searchParams.set("destination", `${destination.location.latitude},${destination.location.longitude}`);
+
+        return url.href;
+    };
+
     if (!plan) return <LoadingModal title="素敵なプランを読み込んでいます"/>
 
     return <Center flexDirection="column">
@@ -72,6 +91,13 @@ const PlanDetail = () => {
                     text="画像で保存する" color="#539565" icon={MdPhotoCamera}
                     onClick={handleOnClickSaveAsImage}
                 />
+                <Link href={generateGoogleMapUrl()} target="_blank" style={{width: "100%"}}>
+                    <PlanActionButton
+                        text="Google Mapで経路を調べる"
+                        color="#0F88E7"
+                        imageUrl="/images/google_map_logo.png"
+                    />
+                </Link>
             </VStack>
         </VStack>
     </Center>
