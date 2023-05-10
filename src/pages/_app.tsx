70827db5143b33e54b223490a7cbefed736642c8
ishizakamasaki <robomasaki2@gmail.com>
2023-05-10 12:59:16 +0900

    refactor: タブの大きさを調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,70 +12,87 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <meta charSet="utf-8"/>
                 <title>poroto</title>
-                <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0, user-scalable=no"/>
-                <meta name="theme-color" content="#F7F5EE"/>
-                <link rel="manifest" href="/manifest.webmanifest"/>
-                <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
-                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>
-                <link rel="icon" type="image/png" href="/favicon/favicon.png"/>
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,maximum-scale=1.0, user-scalable=no"
+                />
+                <meta name="theme-color" content="#F7F5EE" />
+                <link rel="manifest" href="/manifest.webmanifest" />
+                <link
+                    rel="apple-touch-icon"
+                    href="/icons/icon-192x192.png"
+                ></link>
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href="/favicon/favicon.svg"
+                />
+                <link rel="icon" type="image/png" href="/favicon/favicon.png" />
                 {/* ogp */}
-                <meta property="og:title" content="poroto"/>
-                <meta property="og:site_name" content="poroto"/>
-                <meta property="og:description" content="porotoは、あなたの暇な時間を楽しく過ごすための最適なプランを自動的に生成します！"/>
-                <meta property="og:url" content="https://poroto.app/"/>
-                <meta property="og:image" content="https://poroto.app/ogp/ogp.png"/>
-                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="poroto" />
+                <meta property="og:site_name" content="poroto" />
+                <meta
+                    property="og:description"
+                    content="porotoは、あなたの暇な時間を楽しく過ごすための最適なプランを自動的に生成します！"
+                />
+                <meta property="og:url" content="https://poroto.app/" />
+                <meta
+                    property="og:image"
+                    content="https://poroto.app/ogp/ogp.png"
+                />
+                <meta property="og:type" content="website" />
             </Head>
             {/*MEMO:GitHub Actionsでtype checkを実行すると落ちる*/}
             {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
             {/*@ts-ignore*/}
             <style jsx global>
                 {`
-                  html {
-                    font-family: Helvetica, Hiragino Kaku Gothic Pro, Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
-                  }
+                    html {
+                        font-family: Helvetica, Hiragino Kaku Gothic Pro,
+                            Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
+                    }
 
-                  button {
-                    border-style: none;
-                    background-color: transparent;
-                    font-size: inherit;
-                  }
+                    button {
+                        border-style: none;
+                        background-color: transparent;
+                        font-size: inherit;
+                    }
 
-                  button:focus {
-                    outline: none;
-                  }
+                    button:focus {
+                        outline: none;
+                    }
 
-                  html,
-                  body,
-                  #__next {
-                    height: 100%;
-                    margin: 0;
-                    padding: 0;
-                    min-width: 370px;
-                  }
+                    html,
+                    body,
+                    #__next {
+                        height: 100%;
+                        margin: 0;
+                        padding: 0;
+                        min-width: 370px;
+                    }
 
-                  h1,
-                  h2,
-                  h3,
-                  h4,
-                  h5,
-                  h6 {
-                    margin: 0;
-                  }
+                    h1,
+                    h2,
+                    h3,
+                    h4,
+                    h5,
+                    h6 {
+                        margin: 0;
+                    }
 
-                  a {
-                    text-decoration: none;
-                    color: inherit;
-                  }
+                    a {
+                        text-decoration: none;
+                        color: inherit;
+                    }
 
-                  a:hover {
-                    cursor: pointer;
-                  }
+                    a:hover {
+                        cursor: pointer;
+                    }
 
-                  ol,
-                  ul {
-                    list-style: none;
-                  }
+                    ol,
+                    ul {
+                        list-style: none;
+                    }
                 `}
             </style>
             <ChakraProvider>
