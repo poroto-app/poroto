7656dcebc6ab7cad36423469cf044ab1e43bc910
zacker <robomasaki2@gmail.com>
2023-05-13 20:27:56 +0900

    feat(view): 場所が選択されたときにプランを作成する

--- src/pages/places/search.tsx
+++ src/pages/places/search.tsx
@@ -4,11 +4,11 @@ import {PlaceSearchBar} from "src/view/place/PlaceSearchBar";
 import {PlaceSearchResults} from "src/view/place/PlaceSearchResults";
 import {useEffect} from "react";
 import {useAppDispatch} from "src/redux/redux";
-import {
-    reduxPlaceSearchSelector,
-    resetPlaceSearchResults,
-    searchPlacesByQuery,
-} from "src/redux/placeSearch";
+import {reduxPlaceSearchSelector, resetPlaceSearchResults, searchPlacesByQuery,} from "src/redux/placeSearch";
+import {PlaceSearchResult} from "src/domain/models/PlaceSearchResult";
+import {createPlanByPlaceId} from "src/redux/plan";
+import {useRouter} from "next/router";
+import {Routes} from "src/view/constants/router";
 
 const mockPlaces = [
     {
@@ -29,11 +29,17 @@ const mockPlaces = [
 ]
 
 export default function PlaceSearchPage() {
+    const router = useRouter();
     const dispatch = useAppDispatch();
     const {placeSearchResults} = reduxPlaceSearchSelector();
 
     const handleOnSearch = (value: string) => {
         dispatch(searchPlacesByQuery({query: value}));
+    }
+
+    const handleOnClickPlace = (placeSearchResult: PlaceSearchResult) => {
+        dispatch(createPlanByPlaceId({placeId: placeSearchResult.id}));
+        router.push(Routes.plans.create).then()
     }
 
     useEffect(() => {
@@ -48,7 +54,10 @@ export default function PlaceSearchPage() {
             <Box w="100%" px="16px">
                 <PlaceSearchBar onSearch={handleOnSearch}/>
             </Box>
-            <PlaceSearchResults places={placeSearchResults || []}/>
+            <PlaceSearchResults
+                places={placeSearchResults || []}
+                onClickPlace={handleOnClickPlace}
+            />
         </VStack>
     </Layout>
 }
