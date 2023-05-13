30d8559fffba87e19f673577e64b7c70b5d8691e
zacker <robomasaki2@gmail.com>
2023-05-13 12:17:42 +0900

    feat: 場所検索のAPIクライアントを実装

---
+++ src/data/map/GooglePlacesApi.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export class GooglePlacesApi {
+    // SEE: https://developers.google.com/maps/documentation/places/web-service/autocomplete
+    async placeAutoComplete({
+                                input,
+                                language,
+                                radius
+                            }: PlaceAutoCompleteRequest): Promise<google.maps.places.AutocompleteResponse> {
+        const responce = await axios.get("https://maps.googleapis.com/maps/api/place/autocomplete/json", {
+            params: {
+                input,
+                language,
+                radius,
+                key: process.env.GCP_API_KEY,
+            }
+        })
+
+        return responce.data;
+    }
+}
+
+type PlaceAutoCompleteRequest = {
+    input: string;
+    radius: string;
+    language: "ja";
+}
