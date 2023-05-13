33dda0bbf29aabf21c061067d5f4c8265eeb5327
zacker <robomasaki2@gmail.com>
2023-05-13 13:20:00 +0900

    feat: 場所検索ページを作成

---
+++ src/pages/places/search.tsx
@@ -0,0 +1,41 @@
+import {Layout} from "src/view/common/Layout";
+import {Box, VStack} from "@chakra-ui/react";
+import {PlaceSearchBar} from "src/view/place/PlaceSearchBar";
+import {PlaceSearchResults} from "src/view/place/PlaceSearchResults";
+import {useState} from "react";
+import {PlaceSearchResult} from "src/domain/models/PlaceSearchResult";
+
+const mockPlaces = [
+    {
+        id: "1",
+        name: "町田駅",
+        address: "日本、東京都町田市原町田６丁目１２ 町田駅",
+    },
+    {
+        id: "2",
+        name: "町田駅前郵便局",
+        address: "日本、東京都町田市原町田４丁目１−１４ 町田駅前郵便局",
+    },
+    {
+        id: "3",
+        name: "町田駅前通り",
+        address: "日本、東京都町田市原町田２丁目１ 町田駅前通り",
+    }
+]
+
+export default function PlaceSearchPage() {
+    const [places, setPlaces] = useState<PlaceSearchResult[]>(mockPlaces);
+
+    const handleOnSearch = (value: string) => {
+        console.log("Search", value);
+    }
+
+    return <Layout>
+        <VStack w="100%" h="100%" pt="24px" spacing={4}>
+            <Box w="100%" px="16px">
+                <PlaceSearchBar onSearch={handleOnSearch}/>
+            </Box>
+            <PlaceSearchResults places={places}/>
+        </VStack>
+    </Layout>
+}
