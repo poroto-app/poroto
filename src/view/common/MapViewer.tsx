5c8db005f20d1da29842ea11b3f158f3152f0c82
zacker <robomasaki2@gmail.com>
2023-05-20 09:34:27 +0900

    feat(view): 場所をクリックでピン選択できるコンポーネントを作成

--- src/view/common/MapViewer.tsx
+++ src/view/common/MapViewer.tsx
@@ -5,10 +5,11 @@ export type MapViewerProps = {
     zoom: number // SEE: https://developers.google.com/maps/documentation/javascript/overview?hl=ja#zoom-levels
     center?: { lat: number, lng: number }
     loadingPlaceHolder?: JSX.Element,
+    onClick?: (e: google.maps.MapMouseEvent) => void,
     children?: ReactNode
 }
 
-export function MapViewer({zoom, center, loadingPlaceHolder, children}: MapViewerProps) {
+export function MapViewer({zoom, center, loadingPlaceHolder, onClick, children}: MapViewerProps) {
     const {isLoaded} = useLoadScript({
         googleMapsApiKey: process.env.GCP_API_KEY,
     })
@@ -22,6 +23,7 @@ export function MapViewer({zoom, center, loadingPlaceHolder, children}: MapViewe
         zoom={zoom}
         center={center}
         mapContainerStyle={{width: "100%", height: "100%"}}
+        onClick={onClick}
     >
         {children}
     </GoogleMap>
---
+++ src/view/place/MapPinSelector.tsx
@@ -0,0 +1,29 @@
+import {MapViewer} from "src/view/common/MapViewer";
+import {GeoLocation} from "src/domain/models/GeoLocation";
+import {Marker} from "@react-google-maps/api";
+
+export type Props = {
+    center: GeoLocation,
+    onSelectLocation: (location: GeoLocation) => void,
+    pinnedLocation?: GeoLocation,
+}
+
+export function MapPinSelector({center, onSelectLocation, pinnedLocation}: Props) {
+    return <MapViewer
+        center={{lat: center.latitude, lng: center.longitude}}
+        zoom={15}
+        onClick={(e) => onSelectLocation({
+            latitude: e.latLng.lat(),
+            longitude: e.latLng.lng(),
+        })}
+    >
+        {
+            pinnedLocation && <Marker
+                position={{
+                    lat: pinnedLocation.latitude,
+                    lng: pinnedLocation.longitude
+                }}
+            />
+        }
+    </MapViewer>;
+}
