5e97d8ff09f718b5667fe24e43cf4bb5c06b7b10
zacker <robomasaki2@gmail.com>
2023-05-20 18:58:46 +0900

    feat(view): refを受け取れるようにする

--- src/view/plan/PlanScreenShotComponent.tsx
+++ src/view/plan/PlanScreenShotComponent.tsx
@@ -1,60 +1,62 @@
 import styled from "styled-components";
-import { PlanDuration, PlanPrice } from "src/view/plan/PlanSummaryItem";
-import { Plan } from "src/domain/models/Plan";
-import { Place } from "src/domain/models/Place";
+import {PlanDuration, PlanPrice} from "src/view/plan/PlanSummaryItem";
+import {Plan} from "src/domain/models/Plan";
+import {Place} from "src/domain/models/Place";
 import PlanThumbnailStories from "src/stories/plan/PlanThumbnail.stories";
+import {forwardRef, MutableRefObject} from "react";
 
 type Props = {
-	plan: Plan,
-	money: {
-		start: number
-		end?: number
-	}
+    plan: Plan,
+    money: {
+        start: number
+        end?: number
+    }
+    ref?: MutableRefObject<HTMLDivElement>
 }
 
-export const PlanScreenShotComponent = ({ plan, money }: Props) => {
-	return <Block>
-		<PlanTitle>{plan.title}</PlanTitle>
-		{
-			plan.places.map((place, i) => <PlaceListItem key={i} place={place}/>)
-		}
-		<PlanSummaryContainer>
-			<PlanPrice price={money.start} priceEnd={money.end} />
-			<PlanDuration durationInMinutes={plan.timeInMinutes} />
-		</PlanSummaryContainer>
-		<img src="/images/poroto.jpg" alt="poroto画像が表示されます。" />
-	</Block>
-}
+export const PlanScreenShotComponent = forwardRef<HTMLDivElement, Props>(({plan, money}, ref) => {
+    return <Block ref={ref}>
+        <PlanTitle>{plan.title}</PlanTitle>
+        {
+            plan.places.map((place, i) => <PlaceListItem key={i} place={place}/>)
+        }
+        <PlanSummaryContainer>
+            <PlanPrice price={money.start} priceEnd={money.end}/>
+            <PlanDuration durationInMinutes={plan.timeInMinutes}/>
+        </PlanSummaryContainer>
+        <img src="/images/poroto.jpg" alt="poroto画像が表示されます。"/>
+    </Block>
+})
 
-const PlaceListItem = ({ place }: { place: Place }) => {
-	return <PlaceContainer>
-		<PlaceName>{place.name}</PlaceName>
-		<Address>{"住所"/*TODO: 住所を指定できるようにする*/}</Address>
-	</PlaceContainer>
+const PlaceListItem = ({place}: { place: Place }) => {
+    return <PlaceContainer>
+        <PlaceName>{place.name}</PlaceName>
+        <Address>{"住所"/*TODO: 住所を指定できるようにする*/}</Address>
+    </PlaceContainer>
 }
 
 const Block = styled.div`
-	display: flex;
-	flex-direction: column;
-	width: 360px;
+  display: flex;
+  flex-direction: column;
+  width: 360px;
 `;
 const PlanSummaryContainer = styled.div`
-	padding: 16px 0;
+  padding: 16px 0;
 `;
 const PlaceContainer = styled.div`
-	border-bottom: 1px solid rgba(0,0,0,.1);
-	padding: 16px;
+  border-bottom: 1px solid rgba(0, 0, 0, .1);
+  padding: 16px;
 `;
 const PlaceName = styled.div`
-	font-size: 16px;
+  font-size: 16px;
 `;
 const Address = styled.div`
-	font-size: 15px;
-	color: #808080;
+  font-size: 15px;
+  color: #808080;
 `;
 const PlanTitle = styled.div`
-	padding: 16px;
-	font-size: 16px;
-	color: #5E6382;
-	border-bottom: 1px solid rgba(0,0,0,.1);
+  padding: 16px;
+  font-size: 16px;
+  color: #5E6382;
+  border-bottom: 1px solid rgba(0, 0, 0, .1);
 `;
