235a15787de393cd75de706df2ba085459d0b416
zacker <robomasaki2@gmail.com>
2023-05-20 18:26:50 +0900

    refactor(view): プランのプレビューを切り出し

--- src/pages/plans/select/[id].tsx
+++ src/pages/plans/select/[id].tsx
@@ -9,6 +9,7 @@ import {useRouter} from "next/router";
 import {useAppDispatch} from "src/redux/redux";
 import {PlanThumbnail} from "src/view/plan/PlanThumbnail";
 import {Layout} from "src/view/common/Layout";
+import {PlanPreview} from "src/view/plan/PlanPreview";
 
 const SelectPlanPage = () => {
 
@@ -42,39 +43,10 @@ const SelectPlanPage = () => {
     return <Layout navBar={<NavBar title="プランを選ぶ"/>}>
         <VStack w="100%" px="16px" spacing={16} py="16px">
             {
-                (plansCreated || []).map((plan, i) => <Link
-                        key={i} href={"/plans/" + plan.id}
-                        style={{width: "100%", maxWidth: "600px"}}
-                    >
-                        <VStack w="100%">
-                            <PlanThumbnail imageUrls={plan.places.flatMap((place) => place.imageUrls)}/>
-                            <VStack w="100%" alignItems="flex-start" spacing={1}>
-                                <Text fontWeight="bold" fontSize="1.25rem">{plan.title}</Text>
-                                <HStack w="100%" justifyContent="flex-start">
-                                    {/* TODO: 最初の地点までの徒歩時間を移動距離を表示 */}
-                                    <TagContainer tag={`${plan.timeInMinutes.toFixed(0)}分`}>
-                                        <Icon w="24px" h="24px" color="#539565" as={MdDirectionsWalk}/>
-                                    </TagContainer>
-                                    {plan.tags.map((tag, i) => <TagContainer key={i} tag={tag.content}/>)}
-                                </HStack>
-                            </VStack>
-                        </VStack>
-                    </Link>
-                )
+                plansCreated.map((plan, i) => <PlanPreview plan={plan} key={i}/>)
             }
         </VStack>
     </Layout>
-}
-
-const TagContainer: FC<{ tag: string }> = ({tag, children}) => {
-    return <HStack
-        spacing={1} alignItems="center" justifyContent="center"
-        border="1px solid rgba(0, 0, 0, .1)" borderRadius="5px"
-        h="100%" px="4px" py="2px"
-    >
-        {children}
-        <Text>{tag}</Text>
-    </HStack>
 }
 
 export default SelectPlanPage;
