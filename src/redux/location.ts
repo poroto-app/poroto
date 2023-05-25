d8c3ef13d7e3a529b42b17883d4081f95f08c551
ishizakamasaki <robomasaki2@gmail.com>
2023-05-25 09:32:18 +0900

    feat: formatterを適用

--- src/redux/location.ts
+++ src/redux/location.ts
@@ -1,35 +1,39 @@
-import {useSelector} from "react-redux";
-import {RootState} from "src/redux/redux";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {GeoLocation} from "src/domain/models/GeoLocation";
+import { useSelector } from "react-redux";
+import { RootState } from "src/redux/redux";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { GeoLocation } from "src/domain/models/GeoLocation";
 
 export type LocationState = {
-    currentLocation: GeoLocation | null,
-    searchLocation: GeoLocation | null,
-}
+    currentLocation: GeoLocation | null;
+    searchLocation: GeoLocation | null;
+};
 
 const initialState: LocationState = {
     currentLocation: null,
     searchLocation: null,
-}
+};
 
 export const slice = createSlice({
-    name: 'location',
+    name: "location",
     initialState,
     reducers: {
-        setCurrentLocation: (state, {payload}: PayloadAction<{ currentLocation: GeoLocation }>) => {
+        setCurrentLocation: (
+            state,
+            { payload }: PayloadAction<{ currentLocation: GeoLocation }>
+        ) => {
             state.currentLocation = payload.currentLocation;
         },
-        setSearchLocation: (state, {payload}: PayloadAction<{ searchLocation: GeoLocation }>) => {
+        setSearchLocation: (
+            state,
+            { payload }: PayloadAction<{ searchLocation: GeoLocation }>
+        ) => {
             state.searchLocation = payload.searchLocation;
         },
     },
 });
 
-export const {
-    setCurrentLocation,
-    setSearchLocation,
-} = slice.actions;
+export const { setCurrentLocation, setSearchLocation } = slice.actions;
 
-export const reduxLocationSelector = () => useSelector((state: RootState) => state.location);
+export const reduxLocationSelector = () =>
+    useSelector((state: RootState) => state.location);
 export const locationReducer = slice.reducer;
