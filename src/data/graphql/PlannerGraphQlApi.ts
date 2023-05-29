410b79888c7354d5aa97c56a51ecd2ffafd29bd3
ishizakamasaki <robomasaki2@gmail.com>
2023-05-29 22:00:54 +0900

    feat(data): nullのときにリクエストに含まれないようにする

--- src/data/graphql/PlannerGraphQlApi.ts
+++ src/data/graphql/PlannerGraphQlApi.ts
@@ -24,7 +24,7 @@ export class PlannerGraphQlApi extends GraphQlRepository implements PlannerApi {
                 latitude: request.location.latitude,
                 longitude: request.location.longitude,
                 categories: request.categories,
-                planDuration: request.planDuration,
+                planDuration: request.planDuration ?? undefined,
             },
         });
         return {
