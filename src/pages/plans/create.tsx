b6128514a7d88dbea8fb1319a15b15d323f611ed
zacker <robomasaki2@gmail.com>
2023-06-04 13:21:21 +0900

    feat(view): 興味を聞く画面以外から遷移してきた場合の制御

--- src/pages/plans/create.tsx
+++ src/pages/plans/create.tsx
@@ -13,6 +13,17 @@ export default function CreatePlanPage() {
     const { searchLocation } = reduxLocationSelector();
     const { createPlanSession, categoryAccepted } = reduxPlanSelector();
 
+    // HACK: このページに「戻るボタン」やURLを叩いて直接遷移してきた場合は、さらに前のページに戻す
+    useEffect(() => {
+        if (createPlanSession) {
+            // プラン作成画面から「戻るボタン」で遷移した場合
+            router.back();
+        } else if (!searchLocation) {
+            // URLを直接叩いて遷移してきた場合
+            router.push(Routes.home).then();
+        }
+    }, []);
+
     // TODO: selectorから変数を取得するのに時間がかかるので、Redux内でリクエストを完結させる
     // 指定した場所からプランを作成する
     useEffect(() => {
@@ -28,7 +39,6 @@ export default function CreatePlanPage() {
     // プランが作成されたら、プラン作成画面に遷移する
     useEffect(() => {
         if (!createPlanSession) return;
-
         router.push(Routes.plans.select(createPlanSession)).then();
     }, [createPlanSession]);
 
