9ac81e4fec59bdedc1f43da8122eb2e3eddf39b9
zacker <robomasaki2@gmail.com>
2023-06-04 12:28:02 +0900

    feat(view): ページを一意に識別できるkeyを用いて履歴を管理する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,25 @@
-import { AppProps } from "next/app";
+import {AppProps} from "next/app";
 import Head from "next/head";
-import { ChakraProvider } from "@chakra-ui/react";
-import { Provider } from "react-redux";
-import { reduxStore, useAppDispatch } from "src/redux/redux";
-import { useEffect } from "react";
-import { popHistoryStack, pushHistoryStack } from "src/redux/history";
+import {ChakraProvider} from "@chakra-ui/react";
+import {Provider} from "react-redux";
+import {reduxStore, useAppDispatch} from "src/redux/redux";
+import {useEffect} from "react";
+import {popHistoryStack, pushHistoryStack, reduxHistorySelector} from "src/redux/history";
+import {useRouter} from "next/router";
+import {copyObject} from "src/domain/util/object";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <>
             <Head>
-                <meta charSet="utf-8" />
+                <meta charSet="utf-8"/>
                 <title>poroto</title>
                 <meta
                     name="viewport"
                     content="width=device-width,initial-scale=1,maximum-scale=1.0, user-scalable=no"
                 />
-                <meta name="theme-color" content="#F7F5EE" />
-                <link rel="manifest" href="/manifest.webmanifest" />
+                <meta name="theme-color" content="#F7F5EE"/>
+                <link rel="manifest" href="/manifest.webmanifest"/>
                 <link
                     rel="apple-touch-icon"
                     href="/icons/icon-192x192.png"
@@ -27,77 +29,77 @@ export default function App({ Component, pageProps }: AppProps) {
                     type="image/svg+xml"
                     href="/favicon/favicon.svg"
                 />
-                <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+                <link rel="icon" type="image/png" href="/favicon/favicon.png"/>
                 {/* ogp */}
-                <meta property="og:title" content="poroto" />
-                <meta property="og:site_name" content="poroto" />
+                <meta property="og:title" content="poroto"/>
+                <meta property="og:site_name" content="poroto"/>
                 <meta
                     property="og:description"
                     content="porotoは、あなたの暇な時間を楽しく過ごすための最適なプランを自動的に生成します！"
                 />
-                <meta property="og:url" content="https://poroto.app/" />
+                <meta property="og:url" content="https://poroto.app/"/>
                 <meta
                     property="og:image"
                     content="https://poroto.app/ogp/ogp.png"
                 />
-                <meta property="og:type" content="website" />
+                <meta property="og:type" content="website"/>
             </Head>
             {/*MEMO:GitHub Actionsでtype checkを実行すると落ちる*/}
             {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
             {/*@ts-ignore*/}
             <style jsx global>
                 {`
-                    html {
-                        font-family: Helvetica, Hiragino Kaku Gothic Pro,
-                            Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
-                    }
+                  html {
+                    font-family: Helvetica, Hiragino Kaku Gothic Pro,
+                    Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
+                  }
 
-                    button {
-                        border-style: none;
-                        background-color: transparent;
-                        font-size: inherit;
-                    }
+                  button {
+                    border-style: none;
+                    background-color: transparent;
+                    font-size: inherit;
+                  }
 
-                    button:focus {
-                        outline: none;
-                    }
+                  button:focus {
+                    outline: none;
+                  }
 
-                    html,
-                    body,
-                    #__next {
-                        height: 100%;
-                        margin: 0;
-                        padding: 0;
-                        min-width: 370px;
-                    }
+                  html,
+                  body,
+                  #__next {
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                    min-width: 370px;
+                  }
 
-                    h1,
-                    h2,
-                    h3,
-                    h4,
-                    h5,
-                    h6 {
-                        margin: 0;
-                    }
+                  h1,
+                  h2,
+                  h3,
+                  h4,
+                  h5,
+                  h6 {
+                    margin: 0;
+                  }
 
-                    a {
-                        text-decoration: none;
-                        color: inherit;
-                    }
+                  a {
+                    text-decoration: none;
+                    color: inherit;
+                  }
 
-                    a:hover {
-                        cursor: pointer;
-                    }
+                  a:hover {
+                    cursor: pointer;
+                  }
 
-                    ol,
-                    ul {
-                        list-style: none;
-                    }
+                  ol,
+                  ul {
+                    list-style: none;
+                  }
                 `}
             </style>
             <ChakraProvider>
                 <Provider store={reduxStore}>
-                    <History />
+                    <History/>
                     <Component {...pageProps} />
                 </Provider>
             </ChakraProvider>
@@ -108,24 +110,29 @@ export default function App({ Component, pageProps }: AppProps) {
 // ページ遷移を記録するためのコンポーネント
 const History = () => {
     const dispatch = useAppDispatch();
+    const router = useRouter();
+    const {historyStack} = reduxHistorySelector();
 
     useEffect(() => {
-        let previousUrl = document.location.href;
-
-        window.onpopstate = () => {
-            if (previousUrl && previousUrl !== location.href) {
-                dispatch(popHistoryStack());
-            }
-        };
-
-        document.addEventListener("click", (e) => {
-            if (e.target["tagName"] !== "A") return;
-            const url = e.target["href"];
-            if (previousUrl && previousUrl !== url)
-                dispatch(pushHistoryStack());
-            previousUrl = url;
-        });
+        dispatch(pushHistoryStack({historyKey: history.state.key}));
     }, []);
+
+    useEffect(() => {
+        const handlePopState = (e: PopStateEvent) => {
+            dispatch(popHistoryStack({historyKey: e.state.key}));
+        }
+
+        const handleRouteChange = () => {
+            dispatch(pushHistoryStack({historyKey: history.state.key}));
+        }
+
+        router.events.on("routeChangeComplete", handleRouteChange);
+        window.addEventListener("popstate", handlePopState);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+            window.removeEventListener("popstate", handlePopState);
+        }
+    }, [copyObject(historyStack)]);
 
     return <></>;
 };
--- src/redux/history.ts
+++ src/redux/history.ts
@@ -1,32 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { RootState } from "src/redux/redux";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {useSelector} from "react-redux";
+import {RootState} from "src/redux/redux";
 
 export type HistoryState = {
-    // 一つ前のページがporoto内のページかとうかを判別するための値
-    // ページリンクによる遷移で+1
-    // 戻るボタンによる遷移で-1
-    historyStack: number;
+    historyStack: string[];
 };
 
 const initialState: HistoryState = {
-    historyStack: 0,
+    historyStack: [],
 };
 
 export const slice = createSlice({
     name: "history",
     initialState,
     reducers: {
-        pushHistoryStack: (state) => {
-            state.historyStack += 1;
+        pushHistoryStack: (state, {payload}: PayloadAction<{ historyKey: string }>) => {
+            if (state.historyStack[state.historyStack.length - 1] === "POP") {
+                // POPイベントがあった場合は、POPの値だけを削除し、履歴の追加は行わない
+                state.historyStack.pop();
+                return;
+            }
+            state.historyStack.push(payload.historyKey);
         },
-        popHistoryStack: (state) => {
-            state.historyStack -= 1;
-        },
+        popHistoryStack: (state, {payload}: PayloadAction<{ historyKey: string }>) => {
+            // POPイベント（「戻るボタン」）の場合は、historyStackにhistoryKeyが存在する(過去にそのページを訪れている)
+            // FORWARDイベント（「進むボタン」）の場合は、historyStackにhistoryKeyが存在しない(過去にそのページを訪れたがPOPイベントで削除されている)
+            const isPopEvent = state.historyStack.indexOf(payload.historyKey) !== -1;
+            if(isPopEvent) {
+                // 履歴の最後の要素を削除する
+                state.historyStack.pop();
+                // pushHistoryStack関数にPOPイベントがあったことを伝える
+                state.historyStack.push("POP");
+            }
+        }
     },
 });
 
-export const { pushHistoryStack, popHistoryStack } = slice.actions;
+export const {pushHistoryStack, popHistoryStack} = slice.actions;
 
 export const reduxHistorySelector = () =>
     useSelector((state: RootState) => state.history);
