4f2ac7c980655730d60316f8f450c69f6a4f6b0f
zacker <robomasaki2@gmail.com>
2023-06-04 13:00:06 +0900

    feat(view): ページ遷移の状態を別の変数で保持

--- src/redux/history.ts
+++ src/redux/history.ts
@@ -1,13 +1,22 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { RootState } from "src/redux/redux";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {useSelector} from "react-redux";
+import {RootState} from "src/redux/redux";
+
+export const PageTransitions = {
+    POP: "POP",
+    FORWARD: "FORWARD",
+    CHANGE: "CHANGE",
+}
+export type PageTransition = typeof PageTransitions[keyof typeof PageTransitions];
 
 export type HistoryState = {
     historyStack: string[];
+    transition: PageTransition | null;
 };
 
 const initialState: HistoryState = {
     historyStack: [],
+    transition: null,
 };
 
 export const slice = createSlice({
@@ -16,18 +25,19 @@ export const slice = createSlice({
     reducers: {
         pushHistoryStack: (
             state,
-            { payload }: PayloadAction<{ historyKey: string }>
+            {payload}: PayloadAction<{ historyKey: string }>
         ) => {
-            if (state.historyStack[state.historyStack.length - 1] === "POP") {
-                // POPイベントがあった場合は、POPの値だけを削除し、履歴の追加は行わない
-                state.historyStack.pop();
+            if (state.transition === PageTransitions.POP) {
+                // POPイベントがあった場合は、履歴の追加は行わない
+                state.transition = null;
                 return;
             }
             state.historyStack.push(payload.historyKey);
+            state.transition = PageTransitions.CHANGE;
         },
         popHistoryStack: (
             state,
-            { payload }: PayloadAction<{ historyKey: string }>
+            {payload}: PayloadAction<{ historyKey: string }>
         ) => {
             // POPイベント（「戻るボタン」）の場合は、historyStackにhistoryKeyが存在する(過去にそのページを訪れている)
             // FORWARDイベント（「進むボタン」）の場合は、historyStackにhistoryKeyが存在しない(過去にそのページを訪れたがPOPイベントで削除されている)
@@ -37,13 +47,15 @@ export const slice = createSlice({
                 // 履歴の最後の要素を削除する
                 state.historyStack.pop();
                 // pushHistoryStack関数にPOPイベントがあったことを伝える
-                state.historyStack.push("POP");
+                state.transition = PageTransitions.POP;
+            } else {
+                state.transition = PageTransitions.FORWARD;
             }
         },
     },
 });
 
-export const { pushHistoryStack, popHistoryStack } = slice.actions;
+export const {pushHistoryStack, popHistoryStack} = slice.actions;
 
 export const reduxHistorySelector = () =>
     useSelector((state: RootState) => state.history);
