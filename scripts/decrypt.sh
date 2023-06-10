8c5b8b090d8fe6d2537b6addcfb19fc6d6d5797b
zacker <robomasaki2@gmail.com>
2023-06-10 20:51:24 +0900

    feat(script): 環境ごとに復号化できるようにする

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,19 +1,53 @@
-# ローカルで実行するときはこの行のコメントアウトを外す
-git clone --depth 1 git@github.com:poroto-app/infrastructure.git
+function decrypt() {
+  environment=$1
+  file=$2
 
-crypt_files_development=(".env.development.local")
-
-echo Decrypt Secrets
-for file in "${crypt_files_development[@]}"
-do
-  echo "[Development] Decrypting ${file} ..."
+  echo "[${environment}] Decrypting ${file} ..."
   gcloud kms decrypt \
     --location "asia-northeast1" \
     --keyring "poroto_key_ring" \
     --key "poroto_crypt_key" \
-    --plaintext-file "./${file}" \
-    --ciphertext-file "./infrastructure/roles/app/poroto/development/${file}.enc" || exit 1
-  echo ">> Decrypted ${file}!"
+    --plaintext-file "${file}" \
+    --ciphertext-file "./infrastructure/roles/app/poroto/${environment}/${file}.enc" || exit 1
+  echo ">> [${environment}] Decrypted ${file}!"
+}
+
+# infrastructureリポジトリをclone
+if [ ! -d infrastructure ]; then
+  git clone --depth 1 git@github.com:poroto-app/infrastructure.git
+fi
+
+# 環境設定（デフォルトはdevelopment）
+env=$1
+if [ -z "${env}" ]; then
+  env="development"
+fi
+
+# 共通のシークレットを復号化
+secrets=()
+for file in "${secrets[@]}"; do
+  decrypt "production" "${file}"
+done
+
+# 環境ごとのシークレットを復号化
+if [ ! -d src/secrets ]; then
+  mkdir src/secrets
+fi
+
+case "$env" in
+"development")
+  secrets_variable=(".env.development.local" "src/secrets/firebaseConfig.ts")
+  ;;
+"staging")
+  secrets_variable=(".env.staging.local" "src/secrets/firebaseConfig.ts")
+  ;;
+"production")
+  secrets_variable=(".env.production.local" "src/secrets/firebaseConfig.ts")
+  ;;
+esac
+
+for file in "${secrets_variable[@]}"; do
+  decrypt "${env}" "${file}"
 done
 
 rm -rf infrastructure
