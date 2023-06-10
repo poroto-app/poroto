04db452b81f3cef1c0238b1e50b89ab738cedf91
zacker <robomasaki2@gmail.com>
2023-06-10 11:53:28 +0900

    feat(storybook): 場所一覧のstorybookを作成

--- src/stories/mock/plan.ts
+++ src/stories/mock/plan.ts
@@ -8,7 +8,12 @@ export const mockPlan: Plan = {
     places: [
         {
             name: "東京駅",
-            imageUrls: [],
+            imageUrls: [
+                "https://picsum.photos/300/400",
+                "https://picsum.photos/1280/720",
+                "https://picsum.photos/400/600",
+                "https://picsum.photos/400/650",
+            ],
             tags: [],
             location: {
                 latitude: 35.6809591,
@@ -17,8 +22,15 @@ export const mockPlan: Plan = {
         },
         {
             name: "東京駅丸の内駅前広場",
-            imageUrls: [],
-            tags: [],
+            imageUrls: [
+                "https://picsum.photos/400/400",
+                "https://picsum.photos/1280/700",
+                "https://picsum.photos/400/500",
+                "https://picsum.photos/450/600",
+            ],
+            tags: [
+
+            ],
             location: {
                 latitude: 35.681616,
                 longitude: 139.764954,
