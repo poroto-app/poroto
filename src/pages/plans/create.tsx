5f6c918a696237d51bc7705330fb66793a90fc57
zacker <robomasaki2@gmail.com>
2023-06-11 09:16:15 +0900

    feat: firebaseの設定値を.envファイルから取得

--- src/pages/plans/create.tsx
+++ src/pages/plans/create.tsx
@@ -6,7 +6,7 @@ import { Routes } from "src/view/constants/router";
 import { reduxLocationSelector } from "src/redux/location";
 import { useAppDispatch } from "src/redux/redux";
 import { PageTransitions, reduxHistorySelector } from "src/redux/history";
-import {getAnalytics, logEvent} from "@firebase/analytics";
+import { getAnalytics, logEvent } from "@firebase/analytics";
 
 // TODO: 「戻るボタン」、「進むボタン」、「URLで直接ページを開く」場合の対応をしなくてもいいルーティングにする
 export default function CreatePlanPage() {
@@ -25,7 +25,7 @@ export default function CreatePlanPage() {
             // 戻るボタンで遷移してきた場合
             router.back();
         } else if ([null, PageTransitions.CHANGE].includes(transition)) {
-            logEvent(getAnalytics(), 'create_plan');
+            logEvent(getAnalytics(), "create_plan");
 
             // 指定した場所からプランを作成する
             //  change: 画面遷移で来た場合
--- src/view/common/FirebaseProvider.tsx
+++ src/view/common/FirebaseProvider.tsx
@@ -1,13 +1,19 @@
-import {initializeApp} from "firebase/app";
-import {firebaseConfig} from "src/secrets/firebaseConfig";
-import {getAnalytics, isSupported} from "@firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "@firebase/analytics";
 
 export const FirebaseProvider = () => {
-    const app = initializeApp(firebaseConfig);
+    const app = initializeApp({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+    });
 
     isSupported().then((supported) => {
-        if(supported) getAnalytics(app);
-    })
+        if (supported) getAnalytics(app);
+    });
 
-    return <></>
-}
+    return <></>;
+};
