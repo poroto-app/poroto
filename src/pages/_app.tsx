36f49cce81ea48ad9128243eee80bebd25324ce2
takumi <takumi.10930823@gmail.com>
2023-06-15 14:21:11 +0900

    next/script は next/head コンポーネント内で使用すべきではないので外に出した。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -49,12 +49,12 @@ export default function App({ Component, pageProps }: AppProps) {
                     content="https://poroto.app/ogp/ogp.png"
                 />
                 <meta property="og:type" content="website" />
-                <Script
+            </Head>
+            <Script
                     async
                     src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADSENSE_KEY}`}
                     crossOrigin="anonymous"
-                />
-            </Head>
+            />
             {/*MEMO:GitHub Actionsでtype checkを実行すると落ちる*/}
             {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
             {/*@ts-ignore*/}
