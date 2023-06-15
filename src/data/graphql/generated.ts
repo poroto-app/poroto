09996719cfcf33a167d4381d415d0e1c898d2e6d
masakizk <masakizk@users.noreply.github.com>
2023-06-15 08:10:10 +0000

    [bot] Update GraphQL Schema

--- src/data/graphql/generated.ts
+++ src/data/graphql/generated.ts
@@ -23,6 +23,16 @@ export type CachedCreatedPlansInput = {
   session: Scalars['String'];
 };
 
+export type ChangePlacesOrderInPlanInput = {
+  id: Scalars['String'];
+  session: Scalars['String'];
+};
+
+export type ChangePlacesOrderInPlanOutput = {
+  __typename?: 'ChangePlacesOrderInPlanOutput';
+  plan: Plan;
+};
+
 export type CreatePlanByLocationInput = {
   categories?: InputMaybe<Array<Scalars['String']>>;
   createdBasedOnCurrentLocation?: InputMaybe<Scalars['Boolean']>;
@@ -62,9 +72,15 @@ export type MatchInterestsInput = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  changePlacesOrderInPlan: ChangePlacesOrderInPlanOutput;
   createPlanByLocation: CreatePlanByLocationOutput;
   ping: Scalars['String'];
   savePlanFromCandidate: SavePlanFromCandidateOutput;
+};
+
+
+export type MutationChangePlacesOrderInPlanArgs = {
+  input: ChangePlacesOrderInPlanInput;
 };
 
 
@@ -85,6 +101,7 @@ export type MutationSavePlanFromCandidateArgs = {
 export type Place = {
   __typename?: 'Place';
   estimatedStayDuration: Scalars['Int'];
+  id: Scalars['String'];
   location: GeoLocation;
   name: Scalars['String'];
   photos: Array<Scalars['String']>;
@@ -103,6 +120,7 @@ export type Query = {
   __typename?: 'Query';
   cachedCreatedPlans: CachedCreatedPlans;
   matchInterests: InterestCandidate;
+  plan?: Maybe<Plan>;
   version: Scalars['String'];
 };
 
@@ -114,6 +132,11 @@ export type QueryCachedCreatedPlansArgs = {
 
 export type QueryMatchInterestsArgs = {
   input?: InputMaybe<MatchInterestsInput>;
+};
+
+
+export type QueryPlanArgs = {
+  id: Scalars['String'];
 };
 
 export type SavePlanFromCandidateInput = {
