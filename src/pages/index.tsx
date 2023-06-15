7ece39ba6756d200b23fb653764cbc5c592f4412
takumi <takumi.10930823@gmail.com>
2023-06-15 14:07:43 +0900

    バナー広告のコンポネート化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { Container, Text, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { Routes } from "src/view/constants/router";
@@ -9,6 +8,7 @@ import { PlaceSearchButton } from "src/view/place/PlaceSearchButton";
 import { Button } from "src/view/common/Button";
 import { MdOutlinePlace } from "react-icons/md";
 import { setCurrentLocation, setSearchLocation } from "src/redux/location";
+import { BannerAd } from "src/view/add/BannerAd";
 
 const IndexPage = () => {
     const router = useRouter();
@@ -21,29 +21,6 @@ const IndexPage = () => {
         dispatch(setSearchLocation({ searchLocation: currentLocation }));
         await router.push(Routes.plans.interest);
     };
-
-    const StyledDiv = styled.div`
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        text-align: center;
-    `;
-
-    const InnerDiv1 = styled.div`
-        padding: 10px;
-        font-size: 13px;
-        text-align: left;
-    `;
-
-    const InnerDiv2 = styled.div`
-        padding: 10px 10px;
-        border: 1px solid #333;
-    `;
-
-    const StyledIns = styled.ins`
-        display: block;
-        text-align: center;
-    `;
 
     return (
         <>
@@ -59,17 +36,7 @@ const IndexPage = () => {
                 {isLoadingLocation && <Text>現在地を取得中</Text>}
                 {isRejected && <Text>現在地の取得を拒否されました。</Text>}
             </Container>
-            <StyledDiv>
-                <InnerDiv1>スポンサーリンク</InnerDiv1>
-                <InnerDiv2>広告</InnerDiv2>
-                <StyledIns
-                    className="adsbygoogle"
-                    data-ad-layout="in-article"
-                    data-ad-format="fluid"
-                    data-ad-client={process.env.ADSENSE_KEY_ClIENT}
-                    data-ad-slot={process.env.ADSENSE_KEY_SLOT}
-                ></StyledIns>
-            </StyledDiv>
+            <BannerAd/>
         </>
     );
 };
