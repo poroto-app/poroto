d76176f3406bcab2d2f9cbffb5618283ea5d99e4
zacker <robomasaki2@gmail.com>
2023-06-25 10:45:55 +0900

    feat(view): lintでエラーが出ないようにする

--- src/view/common/Theme.tsx
+++ src/view/common/Theme.tsx
@@ -1,61 +1,63 @@
 export function Theme() {
-    // MEMO:GitHub Actionsでtype checkを実行すると落ちる
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     return (
-        <style jsx global>
-            {`
-                html {
-                    font-family: Helvetica, Hiragino Kaku Gothic Pro, Segoe UI,
-                        Yu Gothic, Meiryo, MS PGothic, sans-serif;
-                }
+        <>
+            {/*MEMO:GitHub Actionsでtype checkを実行すると落ちる*/}
+            {/*eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+            {/*@ts-ignore*/}
+            <style jsx global>
+                {`
+                    html {
+                        font-family: Helvetica, Hiragino Kaku Gothic Pro,
+                            Segoe UI, Yu Gothic, Meiryo, MS PGothic, sans-serif;
+                    }
 
-                :root {
-                    --color-brown-400: #ac8e6c;
-                    --color-primary-400: var(--color-brown-400);
-                }
+                    :root {
+                        --color-brown-400: #ac8e6c;
+                        --color-primary-400: var(--color-brown-400);
+                    }
 
-                button {
-                    border-style: none;
-                    background-color: transparent;
-                    font-size: inherit;
-                }
+                    button {
+                        border-style: none;
+                        background-color: transparent;
+                        font-size: inherit;
+                    }
 
-                button:focus {
-                    outline: none;
-                }
+                    button:focus {
+                        outline: none;
+                    }
 
-                html,
-                body,
-                #__next {
-                    height: 100%;
-                    margin: 0;
-                    padding: 0;
-                }
+                    html,
+                    body,
+                    #__next {
+                        height: 100%;
+                        margin: 0;
+                        padding: 0;
+                    }
 
-                h1,
-                h2,
-                h3,
-                h4,
-                h5,
-                h6 {
-                    margin: 0;
-                }
+                    h1,
+                    h2,
+                    h3,
+                    h4,
+                    h5,
+                    h6 {
+                        margin: 0;
+                    }
 
-                a {
-                    text-decoration: none;
-                    color: inherit;
-                }
+                    a {
+                        text-decoration: none;
+                        color: inherit;
+                    }
 
-                a:hover {
-                    cursor: pointer;
-                }
+                    a:hover {
+                        cursor: pointer;
+                    }
 
-                ol,
-                ul {
-                    list-style: none;
-                }
-            `}
-        </style>
+                    ol,
+                    ul {
+                        list-style: none;
+                    }
+                `}
+            </style>
+        </>
     );
 }
