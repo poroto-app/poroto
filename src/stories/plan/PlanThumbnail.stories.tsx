18acc911c75239d25ce8c635486bfc71346229ca
zacker <robomasaki2@gmail.com>
2023-07-01 18:31:40 +0900

    feat(storybook): プランのサムネイルのstorybookを最新版に反映

--- src/stories/plan/PlanThumbnail.stories.tsx
+++ src/stories/plan/PlanThumbnail.stories.tsx
@@ -1,21 +1,28 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import { PlanThumbnail } from "src/view/plan/PlanThumbnail";
 
 export default {
     title: "plan/PlanThumbnail",
     component: PlanThumbnail,
-} as ComponentMeta<typeof PlanThumbnail>;
+    tags: ["autodocs"],
+    parameters: {},
+} as Meta<typeof PlanThumbnail>;
 
-const Template: ComponentStory<typeof PlanThumbnail> = (args) => (
-    <PlanThumbnail {...args} />
-);
+type Story = StoryObj<typeof PlanThumbnail>;
 
-export const PlanThumbnailStoryBook = Template.bind({});
-PlanThumbnailStoryBook.args = {
-    imageUrls: [
-        "https://picsum.photos/300/400",
-        "https://picsum.photos/1280/720",
-        "https://picsum.photos/400/600",
-        "https://picsum.photos/400/600",
-    ],
+export const Primary: Story = {
+    args: {
+        imageUrls: [
+            "https://picsum.photos/300/400",
+            "https://picsum.photos/1280/720",
+            "https://picsum.photos/400/600",
+            "https://picsum.photos/400/600",
+        ],
+    },
+};
+
+export const Empty: Story = {
+    args: {
+        imageUrls: [],
+    },
 };
