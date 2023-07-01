5307696c62027edacaccd4859fffc5c73fcda913
zacker <robomasaki2@gmail.com>
2023-07-01 17:44:04 +0900

    feat(view): リンクに下線が惹かれないようにする

--- src/view/common/NotFound.tsx
+++ src/view/common/NotFound.tsx
@@ -1,27 +1,60 @@
-import {Link} from "@chakra-ui/next-js";
-import {Center, Image, Text, VStack} from "@chakra-ui/react";
-import {Routes} from "src/view/constants/router";
-import {RoundedButton} from "./RoundedButton";
-import {Colors} from "src/view/constants/color";
+import { Link } from "@chakra-ui/next-js";
+import { Center, Image, Text, VStack } from "@chakra-ui/react";
+import { Colors } from "src/view/constants/color";
+import { Routes } from "src/view/constants/router";
+import { RoundedButton } from "./RoundedButton";
 
 export const NotFound = () => {
     return (
         <Center w="100%" h="100%">
-            <VStack h="100%" maxW="600px" w="100%" py="32px" px="16px" justifyContent="space-between" userSelect="none">
+            <VStack
+                h="100%"
+                maxW="600px"
+                w="100%"
+                py="32px"
+                px="16px"
+                justifyContent="space-between"
+                userSelect="none"
+            >
                 <Center flexDirection="column" flex={1} w="100%" px="16px">
-                    <VStack color={Colors.primary["400"]} w="100%" alignItems="flex-start" spacing={0}>
-                        <Text fontSize="160px" lineHeight={1} fontWeight="bold">404</Text>
-                        <Text fontSize="32px" lineHeight={1}>Not Found</Text>
+                    <VStack
+                        color={Colors.primary["400"]}
+                        w="100%"
+                        alignItems="flex-start"
+                        spacing={0}
+                    >
+                        <Text fontSize="160px" lineHeight={1} fontWeight="bold">
+                            404
+                        </Text>
+                        <Text fontSize="32px" lineHeight={1}>
+                            Not Found
+                        </Text>
                     </VStack>
                     <Center w="100%">
-                        <Image  w="100%" objectFit="cover" src="/images/404.png" alt="Not Found"/>
+                        <Image
+                            w="100%"
+                            objectFit="cover"
+                            src="/images/404.png"
+                            alt="Not Found"
+                        />
                     </Center>
-                    <VStack spacing={0} w="100%" alignItems="flex-start" color="#222222">
+                    <VStack
+                        spacing={0}
+                        w="100%"
+                        alignItems="flex-start"
+                        color="#222222"
+                    >
                         <Text>申し訳ございません。</Text>
-                        <Text>お探しのしおりの１ページが見つかりませんでした。</Text>
+                        <Text>
+                            お探しのしおりの１ページが見つかりませんでした。
+                        </Text>
                     </VStack>
                 </Center>
-                <Link href={Routes.home} w="100%">
+                <Link
+                    href={Routes.home}
+                    w="100%"
+                    _hover={{ textDecoration: "none" }}
+                >
                     <RoundedButton>ホームに戻る</RoundedButton>
                 </Link>
             </VStack>
