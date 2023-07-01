17ee220c5cc6b012260499c23757a9c15aa5923e
zacker <robomasaki2@gmail.com>
2023-07-01 17:36:04 +0900

    feat(view): 404ページのスタイルを修正

--- src/view/common/NotFound.tsx
+++ src/view/common/NotFound.tsx
@@ -1,29 +1,26 @@
-import { Link } from "@chakra-ui/next-js";
-import { Center, Image, Text, VStack } from "@chakra-ui/react";
-import { Routes } from "src/view/constants/router";
-import styled from "styled-components";
-import { RoundedButton } from "./RoundedButton";
+import {Link} from "@chakra-ui/next-js";
+import {Center, Image, Text, VStack} from "@chakra-ui/react";
+import {Routes} from "src/view/constants/router";
+import {RoundedButton} from "./RoundedButton";
+import {Colors} from "src/view/constants/color";
 
 export const NotFound = () => {
     return (
-        <Center w="100%">
-            <VStack h="100%" py="24px" px="16px" justifyContent="space-between">
-                <VStack>
-                    <VStack>
-                        <HeaderText>404</HeaderText>
-                        <SubText>Not Found</SubText>
+        <Center w="100%" h="100%">
+            <VStack h="100%" maxW="600px" w="100%" py="32px" px="16px" justifyContent="space-between" userSelect="none">
+                <Center flexDirection="column" flex={1} w="100%" px="16px">
+                    <VStack color={Colors.primary["400"]} w="100%" alignItems="flex-start" spacing={0}>
+                        <Text fontSize="160px" lineHeight={1} fontWeight="bold">404</Text>
+                        <Text fontSize="32px" lineHeight={1}>Not Found</Text>
                     </VStack>
-                    <StyledImageWrapper>
-                        <StyledImage src="/images/404.png" alt="Not Found" />
-                    </StyledImageWrapper>
-                    <Center>
-                        <ErrorMessage>
-                            申し訳ございません
-                            <br />
-                            お探しのしおりの１ページが見つかりませんでした
-                        </ErrorMessage>
+                    <Center w="100%">
+                        <Image  w="100%" objectFit="cover" src="/images/404.png" alt="Not Found"/>
                     </Center>
-                </VStack>
+                    <VStack spacing={0} w="100%" alignItems="flex-start" color="#222222">
+                        <Text>申し訳ございません。</Text>
+                        <Text>お探しのしおりの１ページが見つかりませんでした。</Text>
+                    </VStack>
+                </Center>
                 <Link href={Routes.home} w="100%">
                     <RoundedButton>ホームに戻る</RoundedButton>
                 </Link>
@@ -31,36 +28,3 @@ export const NotFound = () => {
         </Center>
     );
 };
-
-const HeaderText = styled(Text)`
-    font-size: 170px;
-    font-weight: bold;
-    color: #5f553b;
-    font-family: "Roboto", sans-serif;
-    margin: 0;
-`;
-
-const SubText = styled(Text)`
-    font-size: 42px;
-    color: #5f553b;
-    margin: 0;
-`;
-
-const StyledImageWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 12px;
-`;
-
-const StyledImage = styled(Image)`
-    max-height: 196px;
-    width: 100%;
-    object-fit: cover;
-`;
-
-const ErrorMessage = styled(Text)`
-    font-size: 20px;
-    text-align: left;
-    margin: 0;
-`;
