65e84365e4a3955e235665cf9cb633d13b010f5c
zacker <robomasaki2@gmail.com>
2023-08-19 09:33:37 +0900

    feat(storybook): デフォルト画像のリストを追加

--- src/stories/interest/CategorySelect.stories.tsx
+++ src/stories/interest/CategorySelect.stories.tsx
@@ -4,6 +4,26 @@ import { CategorySelect } from "src/view/interest/CategorySelect";
 export default {
     title: "interest/CategorySelect",
     component: CategorySelect,
+    argTypes: {
+        defaultThumbnailUrl: {
+            control: {
+                type: "select",
+            },
+            options: [
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_a_day_at_the_park_re_9kxj.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_a_day_at_the_park_re_9kxj.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_art_lover_re_fn8g.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_book_reading_re_fu2c.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_book_reading_re_fu2c.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_breakfast_psiw.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_camping_noc8.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_coffee_re_x35h.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_fish_bowl_uu88.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_pizza_sharing_wxop.svg",
+                "https://storage.googleapis.com/planner-public-asset-bucket/undraw_shopping_bags_o6w5.svg",
+            ],
+        },
+    },
 };
 
 const Template = ({ category, thumbnail, defaultThumbnailUrl }) => (
