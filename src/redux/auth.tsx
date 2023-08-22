11f903dd485c2b244cf6d6d5ac8098c48de03cb2
zacker <robomasaki2@gmail.com>
2023-08-22 21:50:28 +0900

    feat(redux): user情報を取得するactionを実装

--- src/redux/auth.tsx
+++ src/redux/auth.tsx
@@ -1,18 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { PlannerGraphqlUserApi } from "src/data/graphql/PlannerGraphqlUserApi";
+import {
+    RequestStatus,
+    RequestStatuses,
+} from "src/domain/models/RequestStatus";
 import { User } from "src/domain/models/User";
+import { UserApi } from "src/domain/user/UserApi";
 
 export type AuthState = {
     user: User | null;
+
+    fetchByFirebaseUserStatus: RequestStatus | null;
 };
 
 const initialState: AuthState = {
     user: null,
+
+    fetchByFirebaseUserStatus: null,
 };
+
+type FetchByFirebaseUserProps = {
+    firebaseUserId: string;
+    firebaseToken: string;
+};
+export const fetchByFirebaseUser = createAsyncThunk(
+    "auth/fetchByFirebaseUser",
+    async ({ firebaseUserId, firebaseToken }: FetchByFirebaseUserProps) => {
+        const userApi: UserApi = new PlannerGraphqlUserApi();
+        const { user } = await userApi.fetchByFirebaseUserId({
+            firebaseUserId,
+            firebaseToken,
+        });
+        return {
+            user,
+        };
+    }
+);
 
 export const slice = createSlice({
     name: "auth",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            // Fetch By Firebase User
+            .addCase(fetchByFirebaseUser.pending, (state, action) => {
+                state.fetchByFirebaseUserStatus = RequestStatuses.PENDING;
+            })
+            .addCase(
+                fetchByFirebaseUser.fulfilled,
+                (state, { payload: { user } }) => {
+                    state.fetchByFirebaseUserStatus = RequestStatuses.FULFILLED;
+                    state.user = user;
+                }
+            )
+            .addCase(fetchByFirebaseUser.rejected, (state, action) => {
+                state.fetchByFirebaseUserStatus = RequestStatuses.REJECTED;
+            });
+    },
 });
 
 export const authReducer = slice.reducer;
