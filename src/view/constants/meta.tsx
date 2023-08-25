2017946cafd6e4f870a7f9e4f99e8d56d153f94e
zacker <robomasaki2@gmail.com>
2023-08-25 17:31:41 +0900

    feat(view): ページタイトルや説明を付与

---
+++ src/view/constants/meta.tsx
@@ -0,0 +1,19 @@
+export const PageMetaData = {
+    top: {
+        title: "poroto",
+        description: "暇つぶしプランをあなたの代わりにporotoが作ります",
+    },
+    place: {
+        search: {
+            title: "好きな場所からプランを作る | poroto",
+            description:
+                "お気に入りの場所や観光名所から、思い出に残るプランを作ります",
+        },
+    },
+    plans: {
+        interest: {
+            title: "近場でプランを作る | poroto",
+            description: "今の気分にあわせて近場で楽しめるプランを作ります",
+        },
+    },
+};
