c4624c786149401e44947500486047af1b065be7
zacker <robomasaki2@gmail.com>
2023-08-26 21:38:55 +0900

    refactor(view): format

--- src/stories/interest/CouldNotFindAnyPlace.stories.tsx
+++ src/stories/interest/CouldNotFindAnyPlace.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
-import {CouldNotFindAnyPlace} from "src/view/interest/CouldNotFindAnyPlace";
+import { CouldNotFindAnyPlace } from "src/view/interest/CouldNotFindAnyPlace";
 
 export default {
     title: "interest/CouldNotFindAnyPlace",
--- src/view/interest/CouldNotFindAnyPlace.tsx
+++ src/view/interest/CouldNotFindAnyPlace.tsx
@@ -1,28 +1,32 @@
-import {FailurePage} from "src/view/common/FailurePage";
+import { Link } from "@chakra-ui/next-js";
 import Lost from "src/view/assets/svg/lost.svg";
-import {Link} from "@chakra-ui/next-js";
-import {Routes} from "src/view/constants/router";
-import {RoundedButton} from "src/view/common/RoundedButton";
+import { FailurePage } from "src/view/common/FailurePage";
+import { RoundedButton } from "src/view/common/RoundedButton";
+import { Routes } from "src/view/constants/router";
 
 export function CouldNotFindAnyPlace() {
-    return <FailurePage
-        title="Sorry"
-        statusDescription="近くの場所を探すことができませんでした。"
-        image={<Lost
-            viewBox="0 0 885.20079 708.31655"
-            style={{
-                width: "100%",
-                height: "100%",
-            }}
-        />}
-        actions={
-            <Link
-                href={Routes.home}
-                w="100%"
-                _hover={{ textDecoration: "none" }}
-            >
-                <RoundedButton>ホームに戻る</RoundedButton>
-            </Link>
-        }
-    />;
+    return (
+        <FailurePage
+            title="Sorry"
+            statusDescription="近くの場所を探すことができませんでした。"
+            image={
+                <Lost
+                    viewBox="0 0 885.20079 708.31655"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                    }}
+                />
+            }
+            actions={
+                <Link
+                    href={Routes.home}
+                    w="100%"
+                    _hover={{ textDecoration: "none" }}
+                >
+                    <RoundedButton>ホームに戻る</RoundedButton>
+                </Link>
+            }
+        />
+    );
 }
