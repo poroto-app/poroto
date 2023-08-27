fa840bfcfcb0288064c6d2ccbbe26af49e6d76fc
zacker <robomasaki2@gmail.com>
2023-08-27 11:47:04 +0900

    fix(view): 適切にパラメータが付与されるように修正

--- src/view/constants/router.ts
+++ src/view/constants/router.ts
@@ -2,8 +2,11 @@ export const Routes = {
     home: "/",
     plans: {
         plan: (id: string) => `/plans/${id}`,
-        interest: (byLocation?: boolean) =>
-            "/plans/interest" + (byLocation && "?location=true"),
+        interest: (byLocation?: boolean) => {
+            let url = "/plans/interest";
+            if (byLocation) url += "?location=true";
+            return url;
+        },
         select: (session: string) => `/plans/select/${session}`,
         planCandidate: (session: string, planId: string) =>
             `/plans/select/${session}/${planId}`,
