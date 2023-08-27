b5e7c4273f5f8302a7564a63a5ede87b0176a6fb
zacker <robomasaki2@gmail.com>
2023-08-27 09:39:36 +0900

    feat(view): navbarを表示

--- src/view/common/FailurePage.tsx
+++ src/view/common/FailurePage.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Text, VStack } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { NavBar } from "src/view/common/NavBar";
 import { Colors } from "src/view/constants/color";
 
 export type Props = {
@@ -11,6 +12,8 @@ export type Props = {
     image: ReactNode;
 
     actions?: ReactNode;
+
+    navBar?: boolean;
 };
 
 export function FailurePage({
@@ -20,46 +23,54 @@ export function FailurePage({
     description,
     image,
     actions,
+    navBar,
 }: Props) {
     return (
-        <Center w="100%" h="100%" py="32px" px="16px">
-            <VStack w="100%" h="100%" maxW="600px">
-                <Center flexDirection="column" flex={1} w="100%" px="16px">
-                    <VStack
-                        color={Colors.primary["400"]}
-                        w="100%"
-                        alignItems="flex-start"
-                        spacing={0}
-                    >
-                        {statusMessage && (
-                            <Text fontSize="32px" lineHeight={1}>
-                                {statusMessage}
+        <VStack w="100%" h="100%">
+            {navBar && <NavBar />}
+            <Center w="100%" h="100%" py="32px" px="16px">
+                <VStack w="100%" h="100%" maxW="600px">
+                    <Center flexDirection="column" flex={1} w="100%" px="16px">
+                        <VStack
+                            color={Colors.primary["400"]}
+                            w="100%"
+                            alignItems="flex-start"
+                            spacing={0}
+                        >
+                            {statusMessage && (
+                                <Text fontSize="32px" lineHeight={1}>
+                                    {statusMessage}
+                                </Text>
+                            )}
+                            <Text
+                                fontSize="80px"
+                                lineHeight={1}
+                                fontWeight="bold"
+                            >
+                                {title}
                             </Text>
-                        )}
-                        <Text fontSize="80px" lineHeight={1} fontWeight="bold">
-                            {title}
-                        </Text>
-                        {statusDescription && (
-                            <Text color="rgba(0,0,0,.6)" mt="16px">
-                                {statusDescription}
-                            </Text>
-                        )}
-                        <Center w="100%"></Center>
-                    </VStack>
-                    <Box w="100%" my="32px">
-                        {image}
-                    </Box>
-                    <VStack
-                        spacing={0}
-                        w="100%"
-                        alignItems="center"
-                        color="rgba(0,0,0,.6)"
-                    >
-                        {description}
-                    </VStack>
-                </Center>
-                <VStack w="100%">{actions}</VStack>
-            </VStack>
-        </Center>
+                            {statusDescription && (
+                                <Text color="rgba(0,0,0,.6)" mt="16px">
+                                    {statusDescription}
+                                </Text>
+                            )}
+                            <Center w="100%"></Center>
+                        </VStack>
+                        <Box w="100%" my="32px">
+                            {image}
+                        </Box>
+                        <VStack
+                            spacing={0}
+                            w="100%"
+                            alignItems="center"
+                            color="rgba(0,0,0,.6)"
+                        >
+                            {description}
+                        </VStack>
+                    </Center>
+                    <VStack w="100%">{actions}</VStack>
+                </VStack>
+            </Center>
+        </VStack>
     );
 }
