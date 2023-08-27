23a0c54bbb542af720823c7420391f88da6ed284
zacker <robomasaki2@gmail.com>
2023-08-27 09:46:56 +0900

    refactor(view): コンポーネントを分割

--- src/view/common/FailurePage.tsx
+++ src/view/common/FailurePage.tsx
@@ -2,6 +2,7 @@ import { Box, Center, Text, VStack } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { NavBar } from "src/view/common/NavBar";
 import { Colors } from "src/view/constants/color";
+import {func} from "prop-types";
 
 export type Props = {
     title: string;
@@ -25,42 +26,16 @@ export function FailurePage({
     actions,
     navBar,
 }: Props) {
+    if(navBar=== undefined) navBar = true;
+
     return (
         <VStack w="100%" h="100%">
             {navBar && <NavBar />}
             <Center w="100%" h="100%" py="32px" px="16px">
                 <VStack w="100%" h="100%" maxW="600px">
                     <Center flexDirection="column" flex={1} w="100%" px="16px">
-                        <VStack
-                            color={Colors.primary["400"]}
-                            w="100%"
-                            alignItems="flex-start"
-                            spacing={0}
-                        >
-                            {statusMessage && (
-                                <Text fontSize="32px" lineHeight={1}>
-                                    {statusMessage}
-                                </Text>
-                            )}
-                            <Text
-                                fontSize="80px"
-                                lineHeight={1}
-                                fontWeight="bold"
-                            >
-                                {title}
-                            </Text>
-                            {statusDescription && (
-                                <Text color="rgba(0,0,0,.6)" mt="16px">
-                                    {statusDescription}
-                                </Text>
-                            )}
-                            <Center w="100%"></Center>
-                        </VStack>
-                        <Center w="100%">
-                            <Box w="100%" maxW="450px" my="32px">
-                                {image}
-                            </Box>
-                        </Center>
+                        <Header title={title} statusMessage={statusMessage} statusDescription={statusDescription} />
+                        <ErrorImage image={image} />
                         <VStack
                             spacing={0}
                             w="100%"
@@ -75,4 +50,48 @@ export function FailurePage({
             </Center>
         </VStack>
     );
+}
+
+function Header({
+    title,
+    statusMessage,
+    statusDescription,
+                }: {
+    title: string;
+    statusMessage?: string;
+    statusDescription?: string;
+}) {
+    return <VStack
+        color={Colors.primary["400"]}
+        w="100%"
+        alignItems="flex-start"
+        spacing={0}
+    >
+        {statusMessage && (
+            <Text fontSize="32px" lineHeight={1}>
+                {statusMessage}
+            </Text>
+        )}
+        <Text
+            fontSize="80px"
+            lineHeight={1}
+            fontWeight="bold"
+        >
+            {title}
+        </Text>
+        {statusDescription && (
+            <Text color="rgba(0,0,0,.6)" mt="16px">
+                {statusDescription}
+            </Text>
+        )}
+        <Center w="100%"></Center>
+    </VStack>
+}
+
+function ErrorImage({image}: {image: ReactNode}) {
+    return <Center w="100%">
+        <Box w="100%" maxW="450px" my="32px">
+            {image}
+        </Box>
+    </Center>
 }
