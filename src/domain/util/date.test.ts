4fdc1fac8ad7c1465242d614e7f080c039058198
zacker <robomasaki2@gmail.com>
2023-08-29 21:51:15 +0900

    feat(domain): 時間を丸める関数を作成

--- src/domain/util/date.test.ts
+++ src/domain/util/date.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from "@jest/globals";
-import { DateHelper } from "src/domain/util/date";
+import {describe, expect, test} from "@jest/globals";
+import {DateHelper} from "src/domain/util/date";
 
 describe("formatHHMM", () => {
     const cases: {
@@ -45,6 +45,65 @@ describe("formatHHMM", () => {
                 hour: c.delimiterHour,
                 minute: c.delimiterMinute,
             });
+            expect(actual).toEqual(c.expected);
+        })
+    );
+});
+
+describe("roundMinute", () => {
+    const cases: {
+        name: string;
+        timeInMinute: number;
+        round: number;
+        expected: number;
+    }[] = [
+        {
+            name: "0 min",
+            timeInMinute: 0,
+            round: 60,
+            expected: 0,
+        },
+        {
+            name: "29min rounded by 60min",
+            timeInMinute: 29,
+            round: 60,
+            expected: 0,
+        },
+        {
+            name: "30min rounded by 60min",
+            timeInMinute: 30,
+            round: 60,
+            expected: 60,
+        },
+        {
+            name: "31min rounded by 60min",
+            timeInMinute: 31,
+            round: 60,
+            expected: 60,
+        },
+        {
+            name: "89min rounded by 60min",
+            timeInMinute: 89,
+            round: 60,
+            expected: 60,
+        },
+        {
+            name: "90min rounded by 60min",
+            timeInMinute: 90,
+            round: 60,
+            expected: 120,
+        },
+        {
+            name: "91min rounded by 60min",
+            timeInMinute: 91,
+            round: 60,
+            expected: 120,
+        }
+    ]
+
+    cases.forEach((c) =>
+        test(c.name, () => {
+            const actual = DateHelper.roundMinute(c.timeInMinute, c.round);
             expect(actual).toEqual(c.expected);
         })
     );
