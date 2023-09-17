057d1b1bed2f519a79a21c84ad3599d760c6bfbd
zacker <robomasaki2@gmail.com>
2023-09-17 20:02:28 +0900

    refactor(view): Placeに関するモックデータを分離

---
+++ src/stories/mock/place.ts
@@ -0,0 +1,54 @@
+import { Place } from "src/domain/models/Place";
+
+export const mockPlaces: { [key: string]: Place } = {
+    tokyo: {
+        id: "tokyo",
+        name: "東京駅",
+        imageUrls: [
+            "https://picsum.photos/300/400",
+            "https://picsum.photos/1280/720",
+            "https://picsum.photos/400/600",
+            "https://picsum.photos/400/650",
+        ],
+        tags: [],
+        location: {
+            latitude: 35.6809591,
+            longitude: 139.7673068,
+        },
+        estimatedStayDuration: 60,
+        googlePlaceReviews: [
+            {
+                rating: 4.5,
+                text: "とてもきれいな駅です。",
+                authorName: "山田太郎",
+                authorUrl: "https://example.com",
+                authorPhotoUrl: "https://picsum.photos/200/300",
+                timeInMilliSec: 1600000000000,
+            },
+            {
+                rating: 3.5,
+                text: "混雑しています。",
+                authorName: "鈴木二郎",
+                authorUrl: "https://example.com",
+                authorPhotoUrl: "https://picsum.photos/200/300",
+                timeInMilliSec: 1600000000000,
+            },
+        ],
+    },
+    marunouchi: {
+        id: "marunouchi",
+        name: "東京駅丸の内駅前広場",
+        imageUrls: [
+            "https://picsum.photos/400/400",
+            "https://picsum.photos/1280/700",
+            "https://picsum.photos/400/500",
+            "https://picsum.photos/450/600",
+        ],
+        tags: [],
+        location: {
+            latitude: 35.681616,
+            longitude: 139.764954,
+        },
+        estimatedStayDuration: 60,
+    },
+};
