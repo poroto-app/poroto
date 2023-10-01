e82cf7ed35d5774ed9df03d4ab406307a0eb292c
zacker <robomasaki2@gmail.com>
2023-10-01 17:41:08 +0900

    feat(domain): Placeにカテゴリ情報をもたせる

--- src/domain/models/Place.ts
+++ src/domain/models/Place.ts
@@ -1,6 +1,7 @@
 import { GeoLocation } from "src/domain/models/GeoLocation";
 import { GooglePlaceReview } from "src/domain/models/GooglePlaceReview";
 import { Image } from "src/domain/models/Image";
+import { PlaceCategory } from "src/domain/models/PlaceCategory";
 
 export type Place = {
     id: string;
@@ -10,4 +11,5 @@ export type Place = {
     location: GeoLocation;
     estimatedStayDuration: number;
     googlePlaceReviews?: GooglePlaceReview[];
+    categories: PlaceCategory[];
 };
