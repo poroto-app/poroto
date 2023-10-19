6c3ce37ab93e28dda1bf986e1662540b199ac1c1
takumi <takumi.10930823@gmail.com>
2023-10-19 17:20:16 +0900

    画像のアスペクト比に応じてコンポーネントの高さが変化してしまうのを防ぐ

--- src/view/interest/CategorySelect.tsx
+++ src/view/interest/CategorySelect.tsx
@@ -78,7 +78,17 @@ const Thumbnail = styled.img`
 
 const DefaultThumbnail = ({ imageUrl }: { imageUrl: string }) => {
     return (
-        <Center w="100%" h="100%" px="32px" py="32px">
+        <Center
+            w="100%"
+            h="100%"
+            px="32px"
+            py="32px"
+            position="absolute"
+            top="0"
+            right="0"
+            bottom="0"
+            left="0"
+        >
             <Image src={imageUrl} maxW="600px" maxH="400px" h="100%" w="100%" />
         </Center>
     );
