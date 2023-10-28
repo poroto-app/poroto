f66e56ef778a163645cff9f6e0ab69ff9127925f
zacker <robomasaki2@gmail.com>
2023-10-28 17:19:55 +0900

    feat(view): 新たに場所を追加するための処理をまとめたhooksを作成

---
+++ src/view/hooks/usePlanPlaceAdd.ts
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import { RequestStatuses } from "src/domain/models/RequestStatus";
+import {
+    addPlaceToPlanOfPlanCandidate,
+    fetchPlacesToAddToPlanCandidate,
+    reduxEditPlanCandidateSelector,
+    resetAddPlaceToPlanCandidateState,
+} from "src/redux/editPlanCandidate";
+import { useAppDispatch } from "src/redux/redux";
+
+export const usePlanPlaceAdd = ({
+    planCandidateId,
+    planId,
+}: {
+    planCandidateId: string;
+    planId: string;
+}) => {
+    const dispatch = useAppDispatch();
+    const [isDialogVisible, setIsDialogVisible] = useState(false);
+    const {
+        placesToAdd,
+        requestStatusFetchPlacesToAdd,
+        requestStatusAddPlaceToPlanCandidate,
+    } = reduxEditPlanCandidateSelector();
+
+    const showPlacesToAdd = () => {
+        dispatch(
+            fetchPlacesToAddToPlanCandidate({
+                planCandidateId,
+                planId,
+            })
+        );
+    };
+
+    const onCloseDialog = () => {
+        setIsDialogVisible(false);
+        dispatch(resetAddPlaceToPlanCandidateState());
+    };
+
+    const addPlaceToPlan = ({ placeIdToAdd }: { placeIdToAdd: string }) => {
+        dispatch(
+            addPlaceToPlanOfPlanCandidate({
+                planCandidateId,
+                planId,
+                placeId: placeIdToAdd,
+            })
+        );
+    };
+
+    useEffect(() => {
+        dispatch(resetAddPlaceToPlanCandidateState());
+    }, []);
+
+    useEffect(() => {
+        // エラー時にはダイアログを閉じる
+        if (
+            requestStatusFetchPlacesToAdd === RequestStatuses.REJECTED ||
+            requestStatusAddPlaceToPlanCandidate === RequestStatuses.REJECTED
+        ) {
+            // TODO: エラー処理
+            onCloseDialog();
+            return;
+        }
+
+        // 追加が完了したらダイアログを閉じる
+        if (
+            requestStatusAddPlaceToPlanCandidate === RequestStatuses.FULFILLED
+        ) {
+            onCloseDialog();
+            return;
+        }
+
+        // 並び替えが開始したらダイアログを表示する
+        if (requestStatusFetchPlacesToAdd === RequestStatuses.PENDING) {
+            setIsDialogVisible(true);
+            return;
+        }
+    }, [requestStatusFetchPlacesToAdd, requestStatusAddPlaceToPlanCandidate]);
+
+    return {
+        addPlaceToPlan,
+        onCloseDialogToAddPlace: onCloseDialog,
+        showPlacesToAdd,
+        placesToAdd,
+        isAddingPlace:
+            requestStatusAddPlaceToPlanCandidate === RequestStatuses.PENDING,
+        isDialogToAddPlaceVisible: isDialogVisible,
+    };
+};
