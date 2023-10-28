4553bb59688d5fcb4ac5f5c94c1c7c7f03f17467
zacker <robomasaki2@gmail.com>
2023-10-28 11:51:15 +0900

    refactor(domain): factoryメソッドを切り出し

---
+++ src/domain/factory/Place.ts
@@ -0,0 +1,25 @@
+import { Place } from "src/domain/models/Place";
+import { PlaceEntity } from "src/domain/models/PlaceEntity";
+
+export function createPlaceFromPlaceEntity(entity: PlaceEntity): Place {
+    return {
+        id: entity.id,
+        googlePlaceId: entity.googlePlaceId,
+        name: entity.name,
+        images: entity.images,
+        location: entity.location,
+        estimatedStayDuration: entity.estimatedStayDuration,
+        googlePlaceReviews:
+            entity.googlePlaceReviews?.map((review) => ({
+                rating: review.rating,
+                text: review.text,
+                authorName: review.authorName,
+                authorUrl: review.authorUrl,
+                authorPhotoUrl: review.authorPhotoUrl,
+                timeInMilliSec: review.time,
+            })) ?? null,
+        categories: entity.categories.map((category) => ({
+            id: category.id,
+        })),
+    };
+}
