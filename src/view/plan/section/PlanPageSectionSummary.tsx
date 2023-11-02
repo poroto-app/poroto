9eee647295553864a8166899c095f3c2e2cfc1ca
zacker <iszkmasaki@gmail.com>
2023-11-02 18:24:29 +0900

    Revert "プランの要約に予算を追加"
    This reverts commit 51db7be99fd6d899caa0ee29f35f4997aa6ac0b4.

--- src/view/plan/section/PlanPageSectionSummary.tsx
+++ src/view/plan/section/PlanPageSectionSummary.tsx
@@ -1,30 +1,17 @@
 import { HStack } from "@chakra-ui/react";
-import {
-    PlanSummaryBudget,
-    PlanSummaryDuration,
-} from "src/view/plan/PlanSummary";
+import { PlanSummaryDuration } from "src/view/plan/PlanSummary";
 import { PlanPageSection } from "src/view/plan/section/PlanPageSection";
 
 type Props = {
     planDurationInMinutes: number;
-    planMinBudget: number;
-    planMaxBudget: number;
 };
 
-export function PlanPageSectionSummary({
-    planDurationInMinutes,
-    planMinBudget,
-    planMaxBudget,
-}: Props) {
+export function PlanPageSectionSummary({ planDurationInMinutes }: Props) {
     return (
         <PlanPageSection title="プランの情報">
             <HStack w="100%" overflowX="auto" pb={4}>
                 <PlanSummaryDuration
                     durationInMinutes={planDurationInMinutes}
-                />
-                <PlanSummaryBudget
-                    MinBudget={planMinBudget}
-                    MaxBudget={planMaxBudget}
                 />
             </HStack>
         </PlanPageSection>
