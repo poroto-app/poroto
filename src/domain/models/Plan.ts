208f06552722aea9ae05d57335b7a968ff2495a3
takumi <takumi.10930823@gmail.com>
2023-11-03 08:23:42 +0900

    予算の合計値の計算

--- src/domain/models/Plan.ts
+++ src/domain/models/Plan.ts
@@ -1,6 +1,7 @@
 import { Place } from "src/domain/models/Place";
 import { Transition } from "src/domain/models/Transition";
 import { User } from "src/domain/models/User";
+import { PriceRange } from "./PriceRange";
 
 export type Plan = {
     id: string;
@@ -10,3 +11,28 @@ export type Plan = {
     transitions: Transition[];
     author: User | null;
 };
+
+export function getPlanPriceRange(places: Place[]): PriceRange {
+
+    // const placesWithPrice = places.filter(place => place.priceRange !== null);
+
+    let totalMinPrice = 0;
+    let totalMaxPrice = 0;
+
+    for (const place of places) {
+        if (place.priceRange) {
+            totalMinPrice += place.priceRange.priceRangeMin;
+            totalMaxPrice += place.priceRange.priceRangeMax;
+        }
+    }
+    
+    const totalPriceRange: PriceRange = {
+        priceRangeMin: totalMinPrice,
+        priceRangeMax: totalMaxPrice,
+        googlePriceLevel: places.googlePriceLevel,
+    };
+
+    return totalPriceRange;
+}
+
+
