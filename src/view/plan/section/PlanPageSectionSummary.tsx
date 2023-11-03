f8a9c58c23c4b1e4f902a95fb82a6503b4bb58a9
takumi <takumi.10930823@gmail.com>
2023-11-03 14:44:08 +0900

    [fix] 返り値の変更

--- src/view/plan/section/PlanPageSectionSummary.tsx
+++ src/view/plan/section/PlanPageSectionSummary.tsx
@@ -1,11 +1,10 @@
 import { HStack } from "@chakra-ui/react";
+import { PriceRange } from "src/domain/models/PriceRange";
 import {
     PlanSummaryBudget,
     PlanSummaryDuration,
 } from "src/view/plan/PlanSummary";
 import { PlanPageSection } from "src/view/plan/section/PlanPageSection";
-import { PriceRange } from "src/domain/models/PriceRange";
-
 
 type Props = {
     planDurationInMinutes: number;
@@ -16,19 +15,18 @@ export function PlanPageSectionSummary({
     planDurationInMinutes,
     planRange,
 }: Props) {
-    if (planRange.min === 0 && planRange.max === 0) {
-        return <></>;
-    }
     return (
         <PlanPageSection title="プランの情報">
             <HStack w="100%" overflowX="auto" pb={4}>
                 <PlanSummaryDuration
                     durationInMinutes={planDurationInMinutes}
                 />
-                <PlanSummaryBudget
-                    MinBudget={planRange.min}
-                    MaxBudget={planRange.max}
-                />
+                {(planRange.min !== 0 || planRange.max !== 0) && (
+                    <PlanSummaryBudget
+                        MinBudget={planRange.min}
+                        MaxBudget={planRange.max}
+                    />
+                )}
             </HStack>
         </PlanPageSection>
     );
