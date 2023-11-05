6e088264404f5aa63265d959556ca02a330345af
zacker <robomasaki2@gmail.com>
2023-11-05 20:28:25 +0900

    feat(view): paginationを追加

--- src/view/plandetail/PlaceInfoTabPanelReviews.tsx
+++ src/view/plandetail/PlaceInfoTabPanelReviews.tsx
@@ -1,4 +1,4 @@
-import { Center, HStack, Icon } from "@chakra-ui/react";
+import { Box, Center, HStack, Icon, VStack } from "@chakra-ui/react";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/splide/css";
 import { useRef, useState } from "react";
@@ -28,8 +28,6 @@ export const PlaceInfoTabPanelReviews = ({ googlePlaceReviews }: Props) => {
             ref={refSplide}
             options={{
                 arrows: false,
-                pagination: false,
-                height: "100%",
                 flickMaxPages: 1,
                 flickPower: 10,
                 gap: "16px",
@@ -37,35 +35,48 @@ export const PlaceInfoTabPanelReviews = ({ googlePlaceReviews }: Props) => {
             hasTrack={false}
             onMove={(splide) => setCurrentPage(splide.index)}
         >
-            <HStack w="100%" h="100%" alignItems="center" flex={1}>
-                <PageButton
-                    icon={MdArrowBackIos}
-                    disabled={currentPage === 0}
-                    onClick={() => refSplide.current?.go("<")}
+            <VStack w="100%" h="100%" spacing={0}>
+                <HStack
+                    w="100%"
+                    h="100%"
+                    alignItems="center"
+                    flex={1}
+                    overflow="hidden"
+                >
+                    <PageButton
+                        icon={MdArrowBackIos}
+                        disabled={currentPage === 0}
+                        onClick={() => refSplide.current?.go("<")}
+                    />
+                    <SplideTrack>
+                        {reviews.map((review, index) => {
+                            return (
+                                <SplideSlide
+                                    key={index}
+                                    style={{ height: "100%", width: "100%" }}
+                                >
+                                    <PlaceReview
+                                        text={review.text}
+                                        authorName={review.authorName}
+                                        authorUrl={review.authorUrl}
+                                        authorPhotoUrl={review.authorPhotoUrl}
+                                    />
+                                </SplideSlide>
+                            );
+                        })}
+                    </SplideTrack>
+                    <PageButton
+                        icon={MdArrowForwardIos}
+                        disabled={currentPage === reviews.length - 1}
+                        onClick={() => refSplide.current?.go(">")}
+                    />
+                </HStack>
+                <Box
+                    as="ul"
+                    className="splide__pagination"
+                    style={{ position: "initial" }}
                 />
-                <SplideTrack>
-                    {reviews.map((review, index) => {
-                        return (
-                            <SplideSlide
-                                key={index}
-                                style={{ height: "100%", width: "100%" }}
-                            >
-                                <PlaceReview
-                                    text={review.text}
-                                    authorName={review.authorName}
-                                    authorUrl={review.authorUrl}
-                                    authorPhotoUrl={review.authorPhotoUrl}
-                                />
-                            </SplideSlide>
-                        );
-                    })}
-                </SplideTrack>
-                <PageButton
-                    icon={MdArrowForwardIos}
-                    disabled={currentPage === reviews.length - 1}
-                    onClick={() => refSplide.current?.go(">")}
-                />
-            </HStack>
+            </VStack>
         </SplideContainer>
     );
 };
@@ -100,7 +111,7 @@ const SplideContainer = styled(Splide)`
     height: 100%;
     overflow: hidden;
 
-    & > div > .splide__track {
+    & > div > div > .splide__track {
         width: 100%;
         height: 100%;
     }
