5592b6a3e4ae669eb8618d96298f260f88148920
zacker <iszkmasaki@gmail.com>
2023-11-21 16:36:08 +0900

    refactor: format

--- src/pages/api/basic_auth.ts
+++ src/pages/api/basic_auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function BasicAuth(req: NextApiRequest, res: NextApiResponse) {
-    if(process.env.APP_ENV !== "staging") {
+    if (process.env.APP_ENV !== "staging") {
         // Staging環境以外ではBasic認証を行わない(404を返す)
         res.statusCode = 404;
         res.end();
