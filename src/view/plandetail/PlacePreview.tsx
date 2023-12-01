d94ee47a326fcaf1679a4109147b7b9c08a11211
takumi <takumi.10930823@gmail.com>
2023-12-01 16:57:03 +0900

    format

--- src/view/plandetail/PlacePreview.tsx
+++ src/view/plandetail/PlacePreview.tsx
@@ -238,32 +238,34 @@ const ChipAction = ({
 };
 
 const LikeButton = ({ isLiked, likeCount, handleLikeClick }) => {
-    return <HStack alignItems="center" marginTop="4px">
-        <motion.button
-            onClick={handleLikeClick}
-            style={{
-                marginRight: "10px",
-                border: "none",
-                backgroundColor: "transparent",
-                cursor: "pointer",
-                display: "flex",
-                alignItems: "center",
-                fontSize: "1.6rem",
-            }}
-            whileTap={{ scale: 1.1 }}
-            whileHover={{ scale: 1.2 }}
-        >
-            {isLiked ? (
-                <MdFavorite style={{ color: "red" }} />
-            ) : (
-                <MdFavoriteBorder />
-            )}
-        </motion.button>
-        <Text
-            fontSize="0.8rem"
-            as="h2"
-            fontWeight="bold"
-            color="#222222"
-        >{`いいね！${likeCount.toLocaleString()}件`}</Text>
-    </HStack>
+    return (
+        <HStack alignItems="center" marginTop="4px">
+            <motion.button
+                onClick={handleLikeClick}
+                style={{
+                    marginRight: "10px",
+                    border: "none",
+                    backgroundColor: "transparent",
+                    cursor: "pointer",
+                    display: "flex",
+                    alignItems: "center",
+                    fontSize: "1.6rem",
+                }}
+                whileTap={{ scale: 1.1 }}
+                whileHover={{ scale: 1.2 }}
+            >
+                {isLiked ? (
+                    <MdFavorite style={{ color: "red" }} />
+                ) : (
+                    <MdFavoriteBorder />
+                )}
+            </motion.button>
+            <Text
+                fontSize="0.8rem"
+                as="h2"
+                fontWeight="bold"
+                color="#222222"
+            >{`いいね！${likeCount.toLocaleString()}件`}</Text>
+        </HStack>
+    );
 };
