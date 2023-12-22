71d60411b201a3e4aec3816444ec411c34cd1fdd
zacker <robomasaki2@gmail.com>
2023-12-22 09:36:00 +0900

    feat(data): `PlanCandidate`を利用する

--- src/data/graphql/PlannerGraphQlApi.ts
+++ src/data/graphql/PlannerGraphQlApi.ts
@@ -1,6 +1,5 @@
 import {
     AddPlaceToPlanCandidateDocument,
-    CachedCreatedPlansDocument,
     ChangePlacesOrderInPlanCandidateDocument,
     CreatePlanByLocationDocument,
     CreatePlanByPlaceDocument,
@@ -13,6 +12,7 @@ import {
     NearbyPlaceCategoriesDocument,
     PlacesToAddForPlanOfPlanCandidateDocument,
     PlacesToReplaceForPlanOfPlanCandidateDocument,
+    PlanCandidateDocument,
     PlansByLocationDocument,
     PlansByUserDocument,
     ReplacePlaceOfPlanCandidateDocument,
@@ -177,24 +177,26 @@ export class PlannerGraphQlApi extends GraphQlRepository implements PlannerApi {
         request: FetchCachedCreatedPlansRequest
     ): Promise<FetchCachedCreatedPlansResponse> {
         const { data } = await this.client.query({
-            query: CachedCreatedPlansDocument,
-            variables: { session: request.session },
+            query: PlanCandidateDocument,
+            variables: { planCandidateId: request.session },
         });
 
-        if (!data.cachedCreatedPlans.plans)
+        // TODO: PlanCandidate として return する
+        if (!data.planCandidate) {
             return {
                 plans: null,
                 createdBasedOnCurrentLocation: false,
                 likedPlaceIds: [],
             };
+        }
 
         return {
             createdBasedOnCurrentLocation:
-                data.cachedCreatedPlans.createdBasedOnCurrentLocation,
-            plans: data.cachedCreatedPlans.plans.map((plan) =>
+                data.planCandidate.planCandidate.createdBasedOnCurrentLocation,
+            plans: data.planCandidate.planCandidate.plans.map((plan) =>
                 fromGraphqlPlanEntity(plan)
             ),
-            likedPlaceIds: data.cachedCreatedPlans.likedPlaceIds,
+            likedPlaceIds: data.planCandidate.planCandidate.likedPlaceIds,
         };
     }
 
