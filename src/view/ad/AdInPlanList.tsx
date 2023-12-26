b7864f020a38ccedbb663d34f25629b7f80fff63
zacker <robomasaki2@gmail.com>
2023-12-26 14:37:11 +0900

    feat(view): ページの横幅いっぱいに広告が入るようにする

--- src/view/ad/AdInPlanList.tsx
+++ src/view/ad/AdInPlanList.tsx
@@ -1,12 +1,20 @@
-import { Center, Text } from "@chakra-ui/react";
+import {Box, Center, Text} from "@chakra-ui/react";
 import { GoogleAdsense } from "./GoogleAdsense";
 
 export function AdInPlanList() {
+    return <Box gridColumn="1 / -1">
+        <AdComponent />
+    </Box>
+}
+
+function AdComponent() {
     if (process.env.APP_ENV !== "production") {
         return (
             <Center
                 w="100%"
                 h="100%"
+                minH="300px"
+                maxW="100%"
                 backgroundColor="#EEEEEE"
                 userSelect="none"
             >
