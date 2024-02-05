40958e0b24fd7b49215854d86dbaa035e2d9b12b
zacker <robomasaki2@gmail.com>
2024-02-05 21:26:11 +0900

    feat(refactor): NavBarの高さを切り出し

--- src/view/constants/size.ts
+++ src/view/constants/size.ts
@@ -1,6 +1,9 @@
 export const Size = {
     mainContentWidth: "var(--size-main-content-width)",
     PlaceCardPaddingH: "16px",
+    NavBar: {
+        height: "50px",
+    },
     PlanDetailHeader: {
         maxW: "500px",
         px: "16px",
