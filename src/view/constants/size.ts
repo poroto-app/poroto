dcd2ea54b57fad26fcc4cbb7f8744475632994a0
zacker <robomasaki2@gmail.com>
2024-02-05 20:29:24 +0900

    feat(view): 詳細ページヘッダーの横幅を統一するためのパラメータを追加

--- src/view/constants/size.ts
+++ src/view/constants/size.ts
@@ -1,4 +1,9 @@
 export const Size = {
     mainContentWidth: "var(--size-main-content-width)",
     PlaceCardPaddingH: "16px",
+    PlanDetailHeader: {
+        maxW: "500px",
+        px: "16px",
+        imageH: "300px",
+    },
 };
