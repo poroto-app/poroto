07b9639e4a4ad22c34c1f076950aeadf078663e1
zacker <iszkmasaki@gmail.com>
2024-02-25 12:43:22 +0900

    feat(view): コンポーネントを切り出し

---
+++ src/view/plandetail/header/PlanHeaderPlaceCard.tsx
@@ -0,0 +1,49 @@
+import { Box, HStack, Icon, Text } from "@chakra-ui/react";
+import { MdLocationOn } from "react-icons/md";
+import { getImageSizeOf, ImageSizes } from "src/domain/models/Image";
+import { Place } from "src/domain/models/Place";
+import { ImageWithSkeleton } from "src/view/common/ImageWithSkeleton";
+import { PlaceLikeButton } from "src/view/plandetail/PlaceLikeButton";
+
+type Props = {
+    place: Place;
+    isLiked: boolean;
+    likeCount: number;
+    onUpdateLike: (like: boolean) => void;
+};
+
+export function PlanHeaderPlaceCard({
+    place,
+    isLiked,
+    likeCount,
+    onUpdateLike,
+}: Props) {
+    return (
+        <Box position="relative" w="100%" h="100%">
+            <ImageWithSkeleton
+                src={getImageSizeOf(ImageSizes.Large, place.images[0])}
+                isGoogleImage={place.images[0].isGoogleImage}
+            />
+            <HStack
+                position="absolute"
+                backgroundColor="white"
+                borderRadius="5px"
+                px="4px"
+                py="2px"
+                top="16px"
+                left="16px"
+                userSelect="none"
+            >
+                <Icon as={MdLocationOn} color="#E1A766" />
+                <Text fontSize="14px">{place.name}</Text>
+            </HStack>
+            <Box position="absolute" top={0} right={0} px="16px" py="16px">
+                <PlaceLikeButton
+                    isLiked={isLiked}
+                    likeCount={likeCount}
+                    onUpdateLike={onUpdateLike}
+                />
+            </Box>
+        </Box>
+    );
+}
