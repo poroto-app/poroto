613aca721516c4459a9a79af8e1ef28573425155
zacker <robomasaki2@gmail.com>
2024-03-05 21:11:44 +0900

    feat(view): 複数行のタイトルに対応できるようにする

--- src/view/common/SectionTitle.tsx
+++ src/view/common/SectionTitle.tsx
@@ -3,7 +3,12 @@ import { Text } from "@chakra-ui/react";
 export function SectionTitle({ title }: { title: string }) {
     return (
         <Text fontWeight="bold" fontSize="20px">
-            {title}
+            {title.split("\n").map((line, index) => (
+                <span key={index}>
+                    {line}
+                    <br />
+                </span>
+            ))}
         </Text>
     );
 }
