2a8764c6f6f3c97fc7b251f96d623719e730eee1
takumi <twada698@gmail.com>
2024-03-08 09:37:28 +0900

    写真をアップロードgcsにアップロードできるようにボタンをに機能をもたせた

--- src/view/plandetail/PlaceChipContextAction.tsx
+++ src/view/plandetail/PlaceChipContextAction.tsx
@@ -7,8 +7,8 @@ import {
     MdOutlineFindReplace,
 } from "react-icons/md";
 import { SiGooglemaps, SiInstagram } from "react-icons/si";
+import useUploadImage from "src/view/hooks/useUploadImage";
 import { OnClickHandler } from "src/view/types/handler";
-
 type Props = {
     label: string;
     icon: IconType;
@@ -108,29 +108,26 @@ export const PlaceChipActionGoogleMaps = ({
 };
 
 export const PlaceChipActionCamera = () => {
-    const handleFileInput = (event: Event) => {
-        const target = event.target as HTMLInputElement;
-        const file = target.files?.length > 0 && target.files[0];
-        if (file) {
-            console.log("Selected file:", file);
-        }
-    };
-
-    const openCamera = () => {
-        const input = document.createElement("input");
-        input.type = "file";
-        input.accept = "image/*";
-        // MEMO: カメラが利用できる場合は、カメラを起動する。利用できない場合はファイル選択ダイアログを表示する。
-        input.capture = "environment";
-        input.onchange = handleFileInput;
-        input.click();
-    };
+    const {
+        file,
+        uploadMessage,
+        imageURL,
+        uploadProgress,
+        handleFileChange,
+        handleUpload,
+    } = useUploadImage();
 
     return (
-        <PlaceChipContextAction
-            label="写真を撮る"
-            icon={MdOutlineCameraAlt}
-            onClick={openCamera}
-        />
+        <div>
+            <input
+                type="file"
+                onChange={(e) => handleFileChange(e.target.files[0])}
+            />
+            <PlaceChipContextAction
+                label="写真をアップロード"
+                icon={MdOutlineCameraAlt}
+                onClick={handleUpload}
+            />
+        </div>
     );
 };
