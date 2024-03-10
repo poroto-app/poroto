3213b4ecfa7eee577e439ee5a299957448ba1ffa
takumi <twada698@gmail.com>
2024-03-10 12:57:41 +0900

    ハンドラの削除

--- src/view/plandetail/PlaceChipContextAction.tsx
+++ src/view/plandetail/PlaceChipContextAction.tsx
@@ -1,13 +1,13 @@
 import { Link } from "@chakra-ui/next-js";
-import { HStack, Icon, Text } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { HStack,Icon,Text } from "@chakra-ui/react";
+import { useRef,useState } from "react";
 import { IconType } from "react-icons";
 import {
-    MdOutlineCameraAlt,
-    MdOutlineDeleteOutline,
-    MdOutlineFindReplace,
+MdOutlineCameraAlt,
+MdOutlineDeleteOutline,
+MdOutlineFindReplace
 } from "react-icons/md";
-import { SiGooglemaps, SiInstagram } from "react-icons/si";
+import { SiGooglemaps,SiInstagram } from "react-icons/si";
 import useUploadImage from "src/view/hooks/useUploadImage";
 import DialogUploadImage from "src/view/plancandidate/DialogUploadImage";
 import { OnClickHandler } from "src/view/types/handler";
@@ -125,14 +125,6 @@ export const PlaceChipActionCamera = () => {
         setDialogVisible(true);
     };
 
-    const handleUploadClick = () => {
-        handleUpload();
-    };
-
-    const handleCloseDialog = () => {
-        setDialogVisible(false);
-    };
-
     const imageUrl = file ? URL.createObjectURL(file) : "";
 
     return (
@@ -153,8 +145,8 @@ export const PlaceChipActionCamera = () => {
                 visible={dialogVisible}
                 isUpload={isUpload}
                 imageUrl={imageUrl}
-                onUploadClick={handleUploadClick}
-                onClose={handleCloseDialog}
+                onUploadClick={() => handleUpload()}
+                onClose={() => setDialogVisible(false)}
             />
         </div>
     );
