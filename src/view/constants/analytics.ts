c63a30e656d29488ada6498e2d99dabb49dabf4a
zacker <iszkmasaki@gmail.com>
2024-03-26 20:49:00 +0900

    feat(view): プラン作成イベントを記録

--- src/view/constants/analytics.ts
+++ src/view/constants/analytics.ts
@@ -1,5 +1,7 @@
 export const AnalyticsEvents = {
     CreatePlan: "create_plan",
+    CreatePlanFromCurrentLocation: "create_plan_from_current_location",
+    CreatePlanFromSelectedLocation: "create_plan_from_selected_location",
 };
 export type AnalyticsEvent =
     (typeof AnalyticsEvents)[keyof typeof AnalyticsEvents];
