ecdc92a728676e4763f4be0b882127aa042f39ec
takumi <twada698@gmail.com>
2024-04-03 16:49:54 +0900

    uploadProgress プロップを追加した．

--- src/view/plandetail/PlaceChipContextAction.tsx
+++ src/view/plandetail/PlaceChipContextAction.tsx
@@ -178,6 +178,7 @@ export const PlaceChipActionCamera = ({ placeId }: { placeId: string }) => {
                     })
                 }
                 onClose={() => setDialogVisible(false)}
+                uploadProgress={0}
             />
         </div>
     );
