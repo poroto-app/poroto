06b37237ae05e9ec45f1336c413de7e49cee527a
zacker <iszkmasaki@gmail.com>
2024-04-14 20:45:16 +0900

    feat(domain): 条件があうときだけ値を取得する関数を作成

--- src/domain/util/null.test.ts
+++ src/domain/util/null.test.ts
@@ -1,4 +1,4 @@
-import { notEmpty } from "src/domain/util/null";
+import { notEmpty, when } from "src/domain/util/null";
 
 describe("notEmpty", () => {
     const cases: {
@@ -31,6 +31,35 @@ describe("notEmpty", () => {
     cases.forEach((c) =>
         test(c.name, () => {
             const actual = notEmpty(c.value);
+            expect(actual).toEqual(c.expected);
+        })
+    );
+});
+
+describe("when", () => {
+    const cases: {
+        name: string;
+        condition: boolean;
+        value: number;
+        expected: number | undefined;
+    }[] = [
+        {
+            name: "should return value when condition is true",
+            condition: true,
+            value: 1,
+            expected: 1,
+        },
+        {
+            name: "should return undefined when condition is false",
+            condition: false,
+            value: 1,
+            expected: undefined,
+        },
+    ];
+
+    cases.forEach((c) =>
+        test(c.name, () => {
+            const actual = when(c.condition, c.value);
             expect(actual).toEqual(c.expected);
         })
     );
--- src/domain/util/null.ts
+++ src/domain/util/null.ts
@@ -3,3 +3,11 @@ export function notEmpty<TValue>(
 ): value is TValue {
     return value !== null && value !== undefined;
 }
+
+// 条件がtrueのときだけ、値を返す
+export function when<TValue>(
+    condition: boolean,
+    value: TValue
+): TValue | undefined {
+    return condition ? value : undefined;
+}
