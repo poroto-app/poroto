b3ce1312ae83429d5423795782eb529ff33edefa
zacker <iszkmasaki@gmail.com>
2024-04-26 10:38:18 +0900

    feat(view): 高さを指定できるようにする

--- src/view/common/RoundedDialog.tsx
+++ src/view/common/RoundedDialog.tsx
@@ -3,15 +3,17 @@ import { ReactNode } from "react";
 import { Colors } from "src/view/constants/color";
 
 type Props = {
+    h?: string | number;
     children?: ReactNode;
 };
 
-export function RoundedDialog({ children }: Props) {
+export function RoundedDialog({ h, children }: Props) {
     return (
         <Box
             backgroundColor={Colors.dialog.backgroundColor}
             w="500px"
             maxW="100%"
+            h={h}
             borderRadius="20px"
             overflow="hidden"
         >
