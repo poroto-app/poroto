f99d404133eccdc8587cbd5cfa23e1451296b384
zacker <iszkmasaki@gmail.com>
2024-05-21 20:02:31 +0900

    feat(view): おすすめの場所を表示するためのボタンを作成

---
+++ src/view/place/ShowPlaceRecommendationButton.tsx
@@ -0,0 +1,20 @@
+import { Box, Text } from "@chakra-ui/react";
+
+type Props = {
+    onClick?: () => void;
+};
+
+export function ShowPlaceRecommendationButton({ onClick }: Props) {
+    return (
+        <Box
+            backgroundColor="white"
+            borderRadius="50px"
+            px="8px"
+            py="4px"
+            boxShadow="2px 2px 4px #A2A2A2"
+            onClick={onClick}
+        >
+            <Text color="#2D59C9">おすすめの場所を表示する</Text>
+        </Box>
+    );
+}
