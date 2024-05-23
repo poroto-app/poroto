1bda20816ab51c118dc43d50c02f8d9c0881ea6a
takumi <twada698@gmail.com>
2024-05-23 15:24:34 +0900

    fea(view): コラージュ画像のサイズを固定する

--- src/view/plandetail/CollageTemplate.tsx
+++ src/view/plandetail/CollageTemplate.tsx
@@ -1,4 +1,5 @@
 import { Box, Divider, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { forwardRef } from "react";
 import Logo from "src/view/assets/svg/logo.svg";
 
 type CollagePlace = {
@@ -13,76 +14,107 @@ type CollageProps = {
     introduction: string;
 };
 
-export function CollageTemplate({ title, places, introduction }: CollageProps) {
-    return (
-        <Box padding={16} position="relative" background="white">
-            <Box textAlign="center" mb={4}>
-                <Text
-                    color="rgba(130, 141, 205, 1)"
-                    fontFamily="Inter"
-                    fontWeight="bold"
-                    fontSize="80px"
-                >
-                    {title}
-                </Text>
-            </Box>
-            <Divider borderColor="#A4ABD4" my={4} />
-            {places.map((place, index) => (
-                <Box key={index}>
-                    <HStack
-                        justifyContent="space-between"
-                        alignItems="flex-start"
-                        flexDirection={index % 2 === 0 ? "row" : "row-reverse"}
+export const CollageTemplate = forwardRef<HTMLDivElement, CollageProps>(
+    function CollageTemplateComponent({ title, places, introduction }, ref) {
+        return (
+            <VStack
+                ref={ref}
+                w="1080px"
+                h="1920px"
+                padding={16}
+                position="relative"
+                background="white"
+            >
+                <Box textAlign="center" mb={4}>
+                    <Text
+                        color="rgba(130, 141, 205, 1)"
+                        fontFamily="Inter"
+                        fontWeight="bold"
+                        fontSize="80px"
                     >
-                        <Image
-                            src={place.imageUrl}
-                            alt={`Collage Image ${index}`}
-                            borderRadius="lg"
-                            boxSize="400px"
-                        />
-                        <VStack alignItems="flex-start" spacing={0}>
-                            <Text
-                                color="rgba(130, 141, 205, 1)"
-                                fontFamily="Inter"
-                                fontWeight="bold"
-                                fontSize="56px"
-                            >
-                                {place.name}
-                            </Text>
-                            <Text
-                                color="rgba(164, 168, 212, 1)"
-                                fontFamily="Inter"
-                                fontWeight="bold"
-                                fontSize="32px"
-                            >
-                                滞在時間：{place.duration}分
-                            </Text>
-                        </VStack>
-                    </HStack>
-                    {index !== places.length - 1 && (
-                        <Divider borderColor="#A4ABD4" my={4} />
-                    )}
+                        {title}
+                    </Text>
                 </Box>
-            ))}
-            <Divider borderColor="#A4ABD4" my={4} />
-            <HStack spacing={8} alignItems="center">
-                <Text
-                    color="rgba(164, 168, 212, 1)"
-                    fontFamily="Inter"
-                    fontWeight="bold"
-                    fontSize="32px"
-                >
-                    {introduction}
-                </Text>
-                <Logo
-                    viewBox="0 0 315 75"
-                    style={{
-                        bottom: 0,
-                        right: 0,
-                        position: "absolute",
-                    }}
-                />
-            </HStack>
-        </Box>
-    );
-}
+                <Divider borderColor="#A4ABD4" my={4} />
+                <VStack w="100%" flex={1} spacing="32px" divider={<Divider />}>
+                    {places.map((place, index) => (
+                        <HStack
+                            spacing="32px"
+                            key={index}
+                            flex={1}
+                            w="100%"
+                            justifyContent="space-between"
+                            alignItems="flex-start"
+                            flexDirection={
+                                index % 2 === 0 ? "row" : "row-reverse"
+                            }
+                        >
+                            <Box
+                                h="100%"
+                                w="100%"
+                                overflow="hidden"
+                                position="relative"
+                                flex={1}
+                            >
+                                <Image
+                                    position="absolute"
+                                    top={0}
+                                    left={0}
+                                    right={0}
+                                    bottom={0}
+                                    h="100%"
+                                    w="100%"
+                                    objectFit="cover"
+                                    src={place.imageUrl}
+                                    alt={`Collage Image ${index}`}
+                                    borderRadius="lg"
+                                />
+                            </Box>
+                            <VStack
+                                alignItems="flex-start"
+                                spacing={0}
+                                flex={1}
+                            >
+                                <Text
+                                    color="rgba(130, 141, 205, 1)"
+                                    fontFamily="Inter"
+                                    fontWeight="bold"
+                                    fontSize="56px"
+                                >
+                                    {place.name}
+                                </Text>
+                                <Text
+                                    color="rgba(164, 168, 212, 1)"
+                                    fontFamily="Inter"
+                                    fontWeight="bold"
+                                    fontSize="32px"
+                                >
+                                    滞在時間：{place.duration}分
+                                </Text>
+                            </VStack>
+                        </HStack>
+                    ))}
+                </VStack>
+                <Divider borderColor="#A4ABD4" my={4} />
+                <HStack spacing={8} alignItems="center">
+                    <Text
+                        color="rgba(164, 168, 212, 1)"
+                        fontFamily="Inter"
+                        fontWeight="bold"
+                        fontSize="32px"
+                    >
+                        {introduction}
+                    </Text>
+                    <Logo
+                        viewBox="0 0 315 75"
+                        style={{
+                            bottom: 0,
+                            right: 0,
+                            position: "absolute",
+                        }}
+                    />
+                </HStack>
+            </VStack>
+        );
+    }
+);
