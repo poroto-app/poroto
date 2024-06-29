e4298e45b83c3f56f9111dedb4ea43419aa31ff2
zacker <iszkmasaki@gmail.com>
2024-06-29 18:28:34 +0900

    feat(storybook): i18nに対応

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,33 +1,43 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-const config: StorybookConfig = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {}
-  },
-  "core": {},
-  staticDirs: ['../public'],
-  docs: {
-    autodocs: true
-  },
-  webpackFinal: (config) => {
-    if (config.module?.rules) {
-      config.module.rules = config.module.rules.map((rule) => {
-        // HACK: Override SVG loader to not use file-loader
-        if (rule !== '...' && rule.test?.toString().indexOf('svg') !== -1) {
-          rule.exclude = /\.svg$/
-        }
-        return rule
-      })
+import type {StorybookConfig} from "@storybook/nextjs";
 
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-        issuer: /\.tsx$/,
-      })
-    }
-    return config
-  },
+const config: StorybookConfig = {
+    "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    framework: {
+        name: "@storybook/nextjs",
+        options: {}
+    },
+    "core": {},
+    staticDirs: ['../public'],
+    docs: {
+        autodocs: true
+    },
+    webpack: (config) => {
+        if (config.resolve && config.resolve.alias) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                "next-i18next": "react-i18next",
+            }
+        }
+        return config
+    },
+    webpackFinal: (config) => {
+        if (config.module?.rules) {
+            config.module.rules = config.module.rules.map((rule) => {
+                // HACK: Override SVG loader to not use file-loader
+                if (rule !== '...' && rule.test?.toString().indexOf('svg') !== -1) {
+                    rule.exclude = /\.svg$/
+                }
+                return rule
+            })
+
+            config.module.rules.push({
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+                issuer: /\.tsx$/,
+            })
+        }
+        return config
+    },
 };
 export default config;
