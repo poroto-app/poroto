f51557fa17e568e19e4c05496414d744e31d0485
zacker <iszkmasaki@gmail.com>
2024-07-07 15:03:31 +0900

    feat(view): カテゴリの画像をCDNを用いて取得

--- src/view/category/CreatePlanCategory.tsx
+++ src/view/category/CreatePlanCategory.tsx
@@ -1,7 +1,9 @@
-import { Box, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Skeleton, Text, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 import { useState } from "react";
 import { CreatePlanPlaceCategory } from "src/domain/models/CreatePlanPlaceCategory";
 import { Size } from "src/view/constants/size";
+import { appImageLoader } from "src/view/image/appImageLoader";
 
 type Props = {
     category: CreatePlanPlaceCategory;
@@ -20,22 +22,22 @@ export function CreatePlanCategory({ category, onClick }: Props) {
             position="relative"
             onClick={onClick}
         >
-            {/*TDOO：もとにもどす*/}
-            {/*<Skeleton*/}
-            {/*    position="absolute"*/}
-            {/*    top={0}*/}
-            {/*    right={0}*/}
-            {/*    bottom={0}*/}
-            {/*    left={0}*/}
-            {/*    transition="opacity .3s"*/}
-            {/*    opacity={isImageLoading ? 1 : 0}*/}
-            {/*/>*/}
+            <Skeleton
+                position="absolute"
+                top={0}
+                right={0}
+                bottom={0}
+                left={0}
+                transition="opacity .3s"
+                opacity={isImageLoading ? 1 : 0}
+            />
             <Image
-                width={Size.CreatePlanCategory.CategoryImage.width + "px"}
-                height={Size.CreatePlanCategory.CategoryImage.height + "px"}
+                width={Size.CreatePlanCategory.CategoryImage.width}
+                height={Size.CreatePlanCategory.CategoryImage.height}
                 src={category.imageUrl}
                 alt={category.displayName}
                 style={{ objectFit: "cover" }}
+                loader={appImageLoader}
                 onLoad={() => setIsImageLoading(false)}
             />
             <VStack
