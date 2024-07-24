3282b9bb0dc60b80a0b0cc69175b7a9be8687c49
zacker <iszkmasaki@gmail.com>
2024-07-24 16:28:47 +0900

    refactor: format

--- src/view/common/Layout.tsx
+++ src/view/common/Layout.tsx
@@ -16,7 +16,7 @@ export function Layout({
     height = "100%",
     navBar,
     bottomNavigation,
-    maxW = Size.mainContentWidth ,
+    maxW = Size.mainContentWidth,
     header,
     children,
 }: Props) {
@@ -38,7 +38,7 @@ export function Layout({
                 justifyContent="flex-start"
                 position="relative"
             >
-                <Box maxW={maxW+"px"} w="100%" h="100%">
+                <Box maxW={maxW + "px"} w="100%" h="100%">
                     {children}
                 </Box>
             </Center>
