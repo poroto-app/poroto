a68abe2c029878bf91c48c7209fe675cced70458
zacker <robomasaki2@gmail.com>
2024-07-30 16:11:40 +0900

    feat(view): tamagui animationsのバージョンを変更

--- src/view/common/RoundedButton.tsx
+++ src/view/common/RoundedButton.tsx
@@ -1,55 +1,42 @@
-import { ButtonProps } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { Colors } from "src/constant/color";
-import styled from "styled-components";
+import { Button } from "tamagui";
 
 type Props = {
-    onClick?: () => void;
+    w?: number | "100%";
     disabled?: boolean;
     outlined?: boolean;
     children?: ReactNode;
     color?: string;
-} & ButtonProps;
+    onClick?: () => void;
+};
 
 export function RoundedButton({
-    onClick,
+    w = "100%",
     disabled,
     outlined,
     color = Colors.primary["400"],
     children,
+    onClick,
 }: Props) {
     return (
-        <Rounded
-            onClick={onClick}
+        <Button
+            onPress={onClick}
             disabled={disabled ?? false}
-            $outlined={outlined ?? false}
-            $color={color}
+            backgroundColor={disabled ? "#8b8b8b" : outlined ? "white" : color}
+            borderColor={outlined ? Colors.primary["400"] : "none"}
+            color={outlined ? Colors.primary["400"] : "#ffffff"}
+            cursor={disabled ? "default" : "pointer"}
+            fontWeight="bold"
+            height={40}
+            alignItems="center"
+            justifyContent="center"
+            columnGap={8}
+            padding={8}
+            width={w}
+            borderRadius={100}
         >
             {children}
-        </Rounded>
+        </Button>
     );
 }
-
-const Rounded = styled.div<{
-    disabled: boolean;
-    $outlined: boolean;
-    $color: string;
-}>`
-    background-color: ${({ disabled, $outlined, $color }) =>
-        disabled ? "#8b8b8b" : $outlined ? "white" : $color};
-    border-radius: 100px;
-    border: ${({ $outlined }) =>
-        $outlined ? `2px solid ${Colors.primary["400"]}` : "none"};
-    color: ${({ $outlined }) =>
-        $outlined ? Colors.primary["400"] : "#ffffff"};
-    cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
-    display: flex;
-    font-weight: bold;
-    height: 2.5rem;
-    align-items: center;
-    justify-content: center;
-    column-gap: 8px;
-    padding: 4px 8px;
-    width: 100%;
-    text-decoration: none;
-`;
