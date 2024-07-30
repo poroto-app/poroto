5ba63131b8892f3e16739dcc4c437eaa921235d2
zacker <robomasaki2@gmail.com>
2024-07-30 16:38:29 +0900

    fix: lintエラーが出ないようにする

--- src/tamagui/type.ts
+++ src/tamagui/type.ts
@@ -3,5 +3,6 @@ import tamaguiConfig from "src/tamagui/tamagui.config";
 export type AppConfig = typeof tamaguiConfig;
 
 declare module "tamagui" {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface TamaguiCustomConfig extends AppConfig {}
 }
