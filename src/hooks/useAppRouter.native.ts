fa167f91c1aa4add34ccba0f4d55fb5a2770f67f
zacker <robomasaki2@gmail.com>
2024-08-02 16:34:28 +0900

    refactor: Promiseのなかでasyncを利用しない

--- src/hooks/useAppRouter.native.ts
+++ src/hooks/useAppRouter.native.ts
@@ -5,15 +5,9 @@ import { AppRouter } from "src/types/hooks";
 export const useAppRouter = (): AppRouter => {
     const router = useRouter();
     return {
-        push: (url, as, transitionOptions) =>
-            new Promise(async (resolve) => {
-                router.push(url, as, transitionOptions);
-                resolve();
-            }),
-        reload: () =>
-            new Promise(async (resolve) => {
-                await reloadAsync();
-                resolve();
-            }),
+        push: async (url, as, transitionOptions) => {
+            router.push(url, as, transitionOptions);
+        },
+        reload: async () => reloadAsync(),
     };
 };
--- src/hooks/useAppRouter.ts
+++ src/hooks/useAppRouter.ts
@@ -4,15 +4,9 @@ import { AppRouter } from "src/types/hooks";
 export const useAppRouter = (): AppRouter => {
     const router = useRouter();
     return {
-        push: (url, as, transitionOptions) =>
-            new Promise(async (resolve) => {
-                await router.push(url, as, transitionOptions);
-                resolve();
-            }),
-        reload: () =>
-            new Promise((resolve) => {
-                router.reload();
-                resolve();
-            }),
+        push: async (url, as, transitionOptions) => {
+            await router.push(url, as, transitionOptions);
+        },
+        reload: async () => router.reload(),
     };
 };
