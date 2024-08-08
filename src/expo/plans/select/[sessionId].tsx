1b0854ef4ef5fe661a2207416eec5a2299a5064a
zacker <robomasaki2@gmail.com>
2024-08-08 18:20:13 +0900

    feat(view): プラン候補ページをネイティブ対応

--- src/expo/plans/select/[sessionId].tsx
+++ src/expo/plans/select/[sessionId].tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { createParam } from "solito";
+import { Padding } from "src/constant/padding";
+import { Size } from "src/constant/size";
+import { getPlanPriceRange } from "src/domain/models/Plan";
 import { RequestStatuses } from "src/domain/models/RequestStatus";
 import { useAppTranslation } from "src/hooks/useAppTranslation";
+import { usePlaceLikeInPlanCandidate } from "src/hooks/usePlaceLikeInPlanCandidate";
+import { usePlanCandidate } from "src/hooks/usePlanCandidate";
 import { usePlanCandidateSet } from "src/hooks/usePlanCandidateSet";
+import { usePlanCreate } from "src/hooks/usePlanCreate";
+import { reduxNativeSelector } from "src/redux/native";
+import { ButtonWithBlur } from "src/view/common/ButtonWithBlur";
 import { ErrorPage } from "src/view/common/ErrorPage";
 import { LoadingModal } from "src/view/common/LoadingModal";
 import { NotFound } from "src/view/common/NotFound";
+import { SectionTitle } from "src/view/common/SectionTitle";
+import { PlanPageSection } from "src/view/plan/section/PlanPageSection";
 import { PlanCandidatesGallery } from "src/view/plancandidate/PlanCandidatesGallery";
-import { YStack } from "tamagui";
+import { PlanInfoSection } from "src/view/plandetail/PlanInfoSection";
+import { PlanPlaceList } from "src/view/plandetail/PlanPlaceList";
+import { ScrollView, Text, YStack } from "tamagui";
 
 const { useParams } = createParam<{ sessionId?: string }>();
 
@@ -15,10 +27,16 @@ export default function PlanCandidatePage() {
     const { sessionId } = useParams().params;
 
     const { t } = useAppTranslation();
+    const scrollViewRef = useRef<ScrollView>(null);
+    const [scrollY, setScrollY] = useState(0);
     const [selectedPlanIndex, setSelectedPlanIndex] = useState(0);
+    const { screenHeight, safeAreaTop, safeAreaBottom } = reduxNativeSelector();
+    const planCandidateListHeight =
+        screenHeight - (Size.NavBar.height + safeAreaTop + safeAreaBottom);
     const {
         plansCreated,
         placesAvailableForPlan,
+        currentPlanId,
         isCreatingPlan,
         createPlanFromPlaceRequestStatus,
         createPlanFromLocationRequestStatus,
@@ -27,6 +45,7 @@ export default function PlanCandidatePage() {
         createPlanCandidateFromPlace,
     } = usePlanCandidateSet({
         planCandidateSetId: sessionId as string,
+        selectedPlanIndex,
         onCreatedPlanFromPlace: ({ plansCreated }) => {
             // プラン作成完了後、ページトップに移動し、作成されたプランを中央に表示
             setSelectedPlanIndex(plansCreated.length - 1);
@@ -62,8 +81,145 @@ export default function PlanCandidatePage() {
     }
 
     return (
-        <YStack w="100%" h="100%">
-            <PlanCandidatesGallery planCandidates={plansCreated} />
-        </YStack>
+        <ScrollView
+            ref={scrollViewRef}
+            style={{ width: "100%", height: "100%", backgroundColor: "white" }}
+            pagingEnabled={scrollY <= planCandidateListHeight}
+            scrollEventThrottle={16}
+            onScroll={(event) => {
+                setScrollY(event.nativeEvent.contentOffset.y);
+            }}
+        >
+            <YStack
+                alignItems="center"
+                justifyContent="center"
+                w="100%"
+                h={planCandidateListHeight}
+                gap={Padding.p32}
+            >
+                <PlanCandidatesGallery
+                    planCandidates={plansCreated}
+                    isCreating={isCreatingPlan}
+                />
+                <ButtonWithBlur
+                    px={Padding.p16}
+                    py={Padding.p16}
+                    backgroundColor="#84A6FF"
+                    borderRadius={50}
+                    onClick={() =>
+                        scrollViewRef.current?.scrollTo({
+                            y: planCandidateListHeight,
+                        })
+                    }
+                >
+                    <Text color="white" fontWeight="bold" fontSize={18}>
+                        {t("plan:showPlan")}
+                    </Text>
+                </ButtonWithBlur>
+            </YStack>
+            <PlanDetailPage
+                planId={currentPlanId}
+                planCandidateSetId={sessionId}
+                isPlanFooterVisible={true}
+            />
+        </ScrollView>
+    );
+}
+
+export function PlanDetailPage({
+    planId,
+    planCandidateSetId,
+    isPlanFooterVisible,
+    onCreatePlan,
+}: {
+    planId: string | null;
+    planCandidateSetId: string;
+    isPlanFooterVisible: boolean;
+    onCreatePlan?: () => void;
+}) {
+    const { t } = useAppTranslation();
+
+    const {
+        plan,
+        currentLocation,
+        createdBasedOnCurrentLocation,
+        destinationPlacesForPlanCandidate,
+        placeToCreatePlan,
+        onSelectDestinationPlace,
+        onCloseCreatePlanFromPlace,
+        onCreatePlanFromPlace,
+    } = usePlanCandidate({
+        planId: planId,
+    });
+
+    const { likedPlaceIdsInPlanCandidate, updateLikeAtPlace } =
+        usePlaceLikeInPlanCandidate();
+
+    const { createPlan, isCreatingPlan } = usePlanCreate({
+        planCandidateSetId: planCandidateSetId,
+        planId: planId,
+    });
+
+    if (isCreatingPlan) {
+        return <LoadingModal title={t("plan:createPlanInProgressTitle")} />;
+    }
+
+    if (!plan || !planId) {
+        return <></>;
+    }
+
+    return (
+        <>
+            <YStack
+                w="100%"
+                pb={Size.PlanFooter.h}
+                alignItems="center"
+                justifyContent="center"
+            >
+                <YStack
+                    maxWidth={Size.mainContentWidth}
+                    w="100%"
+                    px={0}
+                    py={Padding.p16}
+                    gap={Padding.p16}
+                >
+                    <PlanPageSection
+                        sectionHeader={
+                            <SectionTitle
+                                title={t("plan:planInfo")}
+                                px={Size.PlanDetail.px}
+                            />
+                        }
+                    >
+                        <YStack>
+                            <PlanInfoSection
+                                durationInMinutes={plan.timeInMinutes}
+                                priceRange={getPlanPriceRange(plan.places)}
+                            />
+                        </YStack>
+                    </PlanPageSection>
+                    <PlanPageSection
+                        sectionHeader={
+                            <SectionTitle
+                                title={t("plan:plan")}
+                                px={Size.PlanDetail.px}
+                            />
+                        }
+                    >
+                        <PlanPlaceList
+                            plan={plan}
+                            likePlaceIds={likedPlaceIdsInPlanCandidate}
+                            createdBasedOnCurrentLocation={
+                                createdBasedOnCurrentLocation
+                            }
+                            onClickAddPlace={({ previousPlaceId }) => 0}
+                            onClickShowRelatedPlaces={(placeId) => 0}
+                            onClickDeletePlace={(placeId) => 0}
+                            onUpdateLikeAtPlace={updateLikeAtPlace}
+                        />
+                    </PlanPageSection>
+                </YStack>
+            </YStack>
+        </>
     );
 }
